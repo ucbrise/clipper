8f7dbb0248a76eafc21827da7e7a181f676aed9c
Giulio Zhou <giuliozhou8@gmail.com>
2016-11-08 09:39:54 -0800

    add_endpoint function to add endpoints after initialization

--- src/frontends/src/rest.cpp
+++ src/frontends/src/rest.cpp
@@ -6,7 +6,6 @@
 
 #define BOOST_THREAD_VERSION 4
 #include <boost/thread.hpp>
-#include <boost/thread/future.hpp>
 #define BOOST_SPIRIT_THREADSAFE
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
@@ -16,10 +15,14 @@
 
 #include "server_http.hpp"
 
-using namespace std;
-using namespace clipper;
 using namespace boost::property_tree; 
-typedef SimpleWeb::Server<SimpleWeb::HTTP> HttpServer;
+using clipper::DoubleVector;
+using clipper::FeedbackAck;
+using clipper::Input;
+using clipper::Output;
+using clipper::QueryProcessor;
+using clipper::Response;
+using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
 
 template <typename T>
 std::vector<T> as_vector(ptree const& pt, ptree::key_type const& key) {
@@ -30,72 +33,91 @@ std::vector<T> as_vector(ptree const& pt, ptree::key_type const& key) {
 }
 
 class RequestHandler {
-    HttpServer server;
     public:
-        RequestHandler(QueryProcessor& q, int portno, int num_threads) : server(portno, num_threads) {
-            server.resource["^/predict$"]["POST"]=[&q](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
-                try {
-                    ptree pt;
-                    read_json(request->content, pt);
+        RequestHandler(QueryProcessor& q, int portno, int num_threads) : server(portno, num_threads), qp(q) {}
 
-                    long uid = pt.get<long>("uid");
-                    vector<double> inputs = as_vector<double>(pt, "input");
-
-                    std::shared_ptr<Input> input =
-                        std::make_shared<DoubleVector>(inputs);
-                    auto prediction = q.predict(
-                        {"test", uid, input, 20000, "newest_model", {std::make_pair("m", 1)}});
-                    prediction.then([response](boost::future<Response> f){
-                        Response r = f.get();
-                        stringstream ss; 
-                        ss << "qid:" << r.query_id_ << " predict:" << r.output_->y_hat_;
-                        string content = ss.str();
-                        *response << "HTTP/1.1 200 OK\r\nContent-Length: " << content.length() << "\r\n\r\n" << content;
-                    });
-                } catch (exception&e) {
-                    *response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n" << e.what();
-                }
-            };
-
-            server.resource["^/update$"]["POST"]=[&q](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
-                try {
-                    ptree pt;
-                    read_json(request->content, pt);
-
-                    long uid = pt.get<long>("uid");
-                    vector<double> inputs = as_vector<double>(pt, "input");
-
-                    std::shared_ptr<Input> input =
-                        std::make_shared<DoubleVector>(inputs);
-                    std::shared_ptr<Output> output = 
-                        std::make_shared<Output>(Output(10.0, "model"));
-                    auto update = q.update(
-                        {"label", uid, {std::make_pair(input, output)}, "newest_model", {std::make_pair("m", 1)}});
-                    update.then([response](boost::future<FeedbackAck> f){
-                        FeedbackAck ack = f.get();
-                        stringstream ss;
-                        ss << "Feedback received? " << ack;
-                        string content = ss.str();
-                        *response << "HTTP/1.1 200 OK\r\nContent-Length: " << content.length() << "\r\n\r\n" << content;
-                    });
-                } catch (exception&e) {
-                    *response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n" << e.what();
-                }
+        void add_endpoint(std::string endpoint,
+                          std::function<void(std::shared_ptr<HttpServer::Response>,
+                                        std::shared_ptr<HttpServer::Request>,
+                                        QueryProcessor&)> endpoint_fn) {
+            QueryProcessor& q = qp;
+            server.resource[endpoint]["POST"]=[&q,endpoint_fn](std::shared_ptr<HttpServer::Response> response,
+                                                   std::shared_ptr<HttpServer::Request> request) {
+                endpoint_fn(response, request, q);
             };
         }
 
         void start_listening() {
             HttpServer& s = server;
-            thread server_thread([&s](){
+            std::thread server_thread([&s](){
                 s.start();
             });
 
             server_thread.join();
         }
+    private:
+        HttpServer server;
+        QueryProcessor& qp;
 };
 
 int main() {
   QueryProcessor qp;
   RequestHandler rh(qp, 1337, 8);
+  /* Define predict/update functions, which may be added after initialization */
+  auto predict_fn = [](std::shared_ptr<HttpServer::Response> response,
+                       std::shared_ptr<HttpServer::Request> request,
+                       QueryProcessor& q) {
+       try {
+           ptree pt;
+           read_json(request->content, pt);
+
+           long uid = pt.get<long>("uid");
+           std::vector<double> inputs = as_vector<double>(pt, "input");
+
+           std::shared_ptr<Input> input =
+               std::make_shared<DoubleVector>(inputs);
+           auto prediction = q.predict(
+               {"test", uid, input, 20000, "newest_model", {std::make_pair("m", 1)}});
+           prediction.then([response](boost::future<Response> f){
+               Response r = f.get();
+               std::stringstream ss;
+               ss << "qid:" << r.query_id_ << " predict:" << r.output_->y_hat_;
+               std::string content = ss.str();
+               *response << "HTTP/1.1 200 OK\r\nContent-Length: " << content.length() << "\r\n\r\n" << content;
+           });
+       } catch (std::exception&e) {
+           *response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n" << e.what();
+       }
+  };
+
+  auto update_fn = [](std::shared_ptr<HttpServer::Response> response,
+                      std::shared_ptr<HttpServer::Request> request,
+                      QueryProcessor& q) {
+      try {
+          ptree pt;
+          read_json(request->content, pt);
+
+          long uid = pt.get<long>("uid");
+          std::vector<double> inputs = as_vector<double>(pt, "input");
+
+          std::shared_ptr<Input> input =
+              std::make_shared<DoubleVector>(inputs);
+          std::shared_ptr<Output> output =
+              std::make_shared<Output>(Output(10.0, "model"));
+          auto update = q.update(
+              {"label", uid, {std::make_pair(input, output)}, "newest_model", {std::make_pair("m", 1)}});
+          update.then([response](boost::future<FeedbackAck> f){
+              FeedbackAck ack = f.get();
+              std::stringstream ss;
+              ss << "Feedback received? " << ack;
+              std::string content = ss.str();
+              *response << "HTTP/1.1 200 OK\r\nContent-Length: " << content.length() << "\r\n\r\n" << content;
+          });
+      } catch (std::exception&e) {
+          *response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n" << e.what();
+      }
+  };
+  rh.add_endpoint("^/predict$", predict_fn);
+  rh.add_endpoint("^/update$", update_fn);
   rh.start_listening();
 }
