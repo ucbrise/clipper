73aff9831f45552cad1e1585c0a721b91b5da112
Giulio Zhou <giuliozhou8@gmail.com>
2016-11-29 17:54:36 -0800

    Finished moving all admin functions to admin_rest.cpp

--- src/frontends/src/admin_rest.cpp
+++ src/frontends/src/admin_rest.cpp
@@ -7,7 +7,6 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 #include <clipper/datatypes.hpp>
-#include <clipper/query_processor.hpp>
 #include <server_http.hpp>
 
 using namespace boost::property_tree; 
@@ -17,51 +16,44 @@ using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
 enum InputType { integer_vec, double_vec, byte_vec, float_vec };
 enum OutputType { double_val, int_val };
 
-std::vector<VersionedModelId> parse_model_json(ptree const& pt, ptree::key_type const& key) {
-    std::vector<VersionedModelId> r;
-    for (auto& entry: pt.get_child(key)) {
-        for (auto& model_pair: entry.second) {
-            std::string model_name = model_pair.first;
-            int model_version = model_pair.second.get_value<int>();
-            r.push_back(std::make_pair(model_name, model_version));
-        }
+std::vector<VersionedModelId> parse_model_json(
+    ptree const& pt, ptree::key_type const& key) {
+  std::vector<VersionedModelId> r;
+  for (auto& entry: pt.get_child(key)) {
+    for (auto& model_pair: entry.second) {
+      std::string model_name = model_pair.first;
+      int model_version = model_pair.second.get_value<int>();
+      r.push_back(std::make_pair(model_name, model_version));
     }
-    return r;
+  }
+  return r;
 }
 
 class AdminServer {
-    public:
-        AdminServer(int portno, int num_threads) : server(portno, num_threads) {}
-        AdminServer(std::string address, int portno, int num_threads) : server(address, portno, num_threads) {}
+  public:
+    AdminServer(int portno, int num_threads) : server(portno, num_threads) {
+      AdminServer("0.0.0.0", portno, num_threads);
+    }
 
-        void add_endpoint(std::string endpoint, std::string request_method,
-                          std::function<void(std::shared_ptr<HttpServer::Response>,
-                                             std::shared_ptr<HttpServer::Request>)> endpoint_fn) {
-            server.resource[endpoint][request_method] = endpoint_fn;
-            std::cout << "added " + endpoint + "\n";
-        }
+    AdminServer(std::string address, int portno, int num_threads)
+        : server(address, portno, num_threads) {
+      /* Add function for admin */
+      auto admin_fn = [this](std::shared_ptr<HttpServer::Response> response,
+                             std::shared_ptr<HttpServer::Request> request) {
+        this->application_admin_endpoint(response, request);
+      };
+      add_endpoint("^/admin$", "POST", admin_fn);
+    }
 
-        void start_listening() {
-            HttpServer& s = server;
-            std::thread server_thread([&s](){
-                s.start();
-            });
-
-            server_thread.join();
-        }
-    private:
-        HttpServer server;
-};
-
-/* Assume an input json of the following form:
- * {"name": <new endpoint name>, "models": <array of {"<model_name>": <model_version>} pairs>,
- *  "input_type": <input type string>, "output_type": <output type string>,
- *  "policy": <policy name>, "latency": <latency SLO>}
- */
-void application_endpoint(std::shared_ptr<HttpServer::Response> response,
-                          std::shared_ptr<HttpServer::Request> request,
-                          RequestHandler& rh) {
-    try {
+    /* Assume an input json of the following form:
+     * {"name": <new endpoint name>, "models": <array of {"<model_name>": <model_version>} pairs>,
+     *  "input_type": <input type string>, "output_type": <output type string>,
+     *  "policy": <policy name>, "latency": <latency SLO>}
+     */
+    void application_admin_endpoint(
+        std::shared_ptr<HttpServer::Response> response,
+        std::shared_ptr<HttpServer::Request> request) {
+      try {
         ptree pt;
         read_json(request->content, pt);
 
@@ -74,36 +66,79 @@ void application_endpoint(std::shared_ptr<HttpServer::Response> response,
         InputType input_type;
         OutputType output_type = output_type;
         if (!input_type_name.compare("double_vec")) {
-            input_type = double_vec;
+          input_type = double_vec;
         } else {
-            throw std::invalid_argument(input_type_name + " is not a valid input type");
+          throw std::invalid_argument(
+              input_type_name + " is not a valid input type");
         }
 
         if (!output_type_name.compare("double_val")) {
-            output_type = double_val;
+          output_type = double_val;
         } else {
-            throw std::invalid_argument(output_type_name + " is not a valid output type");
+          throw std::invalid_argument(
+              output_type_name + " is not a valid output type");
         }
 
         /* Specifically, mock out this call */
-        rh.add_application(name, models, input_type, output_type, policy, latency);
-        std::string response_string = "Successfully added endpoint: " + name + "\n";
-        *response << "HTTP/1.1 200 OK\r\nContent-Length: " << response_string.length() << "\r\n\r\n" << response_string;
-    } catch (const ptree_error &e) {
-        *response << "HTTP/1.1 200 OK\r\nContent-Length: " << std::strlen(e.what()) << "\r\n\r\n" << e.what() << "\n";
-    } catch (const std::invalid_argument &e) {
-        *response << "HTTP/1.1 200 OK\r\nContent-Length: " << std::strlen(e.what()) << "\r\n\r\n" << e.what() << "\n";
+        // rh.add_application(name, models, input_type, output_type, policy, latency);
+        send_to_server(name, models, input_type, output_type, policy, latency);
+        std::string response_string = "Added endpoint: " + name + "\n";
+        *response << "HTTP/1.1 200 OK\r\nContent-Length: "
+                  << response_string.length()
+                  << "\r\n\r\n"
+                  << response_string;
+      } catch (const ptree_error &e) {
+        *response << "HTTP/1.1 200 OK\r\nContent-Length: "
+                  << std::strlen(e.what()) << "\r\n\r\n"
+                  << e.what() << "\n";
+      } catch (const std::invalid_argument &e) {
+        *response << "HTTP/1.1 200 OK\r\nContent-Length: "
+                  << std::strlen(e.what()) << "\r\n\r\n"
+                  << e.what() << "\n";
+      }
     }
-}
+
+    void send_to_server(std::string name, std::vector<VersionedModelId> models,
+                        InputType input_type, OutputType output_type,
+                        std::string policy, long latency) {
+        std::stringstream ss;
+        ss << "Name: " << name << std::endl;
+        ss << "Models: ";
+        ss << "[";
+        for (std::pair<std::string, int> model_pair: models) {
+            ss << "{\"" << model_pair.first << "\": " << model_pair.second << "}, ";
+        }
+        ss << "]" << std::endl;
+
+        ss << "InputType: " << input_type << std::endl
+           << "OutputType: " << output_type << std::endl
+           << "Selection Policy: " << policy << std::endl
+           << "Latency Objective: " << latency << std::endl;
+        std::cout << ss.str();
+    }
+
+    void add_endpoint(
+        std::string endpoint,
+        std::string request_method,
+        std::function<void(std::shared_ptr<HttpServer::Response>,
+                           std::shared_ptr<HttpServer::Request>)>
+            endpoint_fn) {
+      server.resource[endpoint][request_method] = endpoint_fn;
+      std::cout << "added " + endpoint + "\n";
+    }
+
+    void start_listening() {
+      HttpServer& s = server;
+      std::thread server_thread([&s](){ s.start(); });
+
+      server_thread.join();
+    }
+  private:
+    HttpServer server;
+};
+
 
 int main() {
   AdminServer admin_server("0.0.0.0", 1337, 1);
-  /* Mock out RequestHandler, for testing */
-  /* Add function for admin */
-  auto application_fn = [&rh](std::shared_ptr<HttpServer::Response> response,
-                              std::shared_ptr<HttpServer::Request> request) {
-    application_endpoint(response, request, rh);
-  };
-  admin_server.add_endpoint("^/admin$", "POST", application_fn);
   admin_server.start_listening();
 }
--- src/frontends/src/rest.cpp
+++ src/frontends/src/rest.cpp
@@ -34,181 +34,148 @@ std::vector<T> as_vector(ptree const& pt, ptree::key_type const& key) {
   return r;
 }
 
-/*
-std::vector<VersionedModelId> parse_model_json(ptree const& pt, ptree::key_type const& key) {
-    std::vector<VersionedModelId> r;
-    for (auto& entry: pt.get_child(key)) {
-        for (auto& model_pair: entry.second) {
-            std::string model_name = model_pair.first;
-            int model_version = model_pair.second.get_value<int>();
-            r.push_back(std::make_pair(model_name, model_version));
-        }
-    }
-    return r;
-} */
-
 std::shared_ptr<Input> decode_input(InputType input_type, ptree& parsed_json) {
-    std::shared_ptr<Input> result;
-    switch(input_type) {
-        case double_vec:
-          {
-            std::vector<double> inputs = as_vector<double>(parsed_json, "input");
-            return std::make_shared<DoubleVector>(inputs);
-          }
-        default:
-            throw std::invalid_argument("input_type is not a valid type");
-    }
+  std::shared_ptr<Input> result;
+  switch(input_type) {
+    case double_vec:
+      {
+        std::vector<double> inputs = as_vector<double>(parsed_json, "input");
+        return std::make_shared<DoubleVector>(inputs);
+      }
+    default:
+      throw std::invalid_argument("input_type is not a valid type");
+  }
 }
 
-std::shared_ptr<Output> decode_output(OutputType output_type, ptree parsed_json) {
-    std::string versioned_model = parsed_json.get<std::string>("model_version");
-    switch(output_type) {
-        case double_val:
-          {
-            double y_hat = parsed_json.get<double>("label");
-            return std::make_shared<Output>(Output(y_hat, versioned_model));
-          }
-        default:
-            throw std::invalid_argument("output_type is not a valid type");
-    }
+Output decode_output(OutputType output_type, ptree parsed_json) {
+  std::string model_name = parsed_json.get<std::string>("model_name");
+  int model_version = parsed_json.get<int>("model_version");
+  VersionedModelId versioned_model = std::make_pair(model_name, model_version);
+  switch(output_type) {
+    case double_val:
+      {
+        double y_hat = parsed_json.get<double>("label");
+        return Output(y_hat, versioned_model);
+      }
+    default:
+      throw std::invalid_argument("output_type is not a valid type");
+  }
 }
 
 class RequestHandler {
-    public:
-        RequestHandler(QueryProcessor& q, int portno, int num_threads) : server(portno, num_threads), qp(q) {}
-        RequestHandler(QueryProcessor& q, std::string address, int portno, int num_threads) : server(address, portno, num_threads), qp(q) {}
+  public:
+    RequestHandler(QueryProcessor& q, int portno, int num_threads)
+        : server(portno, num_threads), qp(q) {}
+    RequestHandler(QueryProcessor& q, std::string address,
+                   int portno, int num_threads)
+        : server(address, portno, num_threads), qp(q) {}
 
-        void add_application(std::string name, std::vector<VersionedModelId> models,
-                             InputType input_type, OutputType output_type, std::string policy, long latency) {
-            QueryProcessor& q = qp;
-            auto predict_fn = [&q,name,input_type,policy,latency,models](std::shared_ptr<HttpServer::Response> response,
-                                                                         std::shared_ptr<HttpServer::Request> request) {
-                try {
-                    ptree pt;
-                    read_json(request->content, pt);
+    void add_application(std::string name, std::vector<VersionedModelId> models,
+                         InputType input_type, OutputType output_type,
+                         std::string policy, long latency) {
+      QueryProcessor& q = qp;
+      auto predict_fn = [&q,name,input_type,policy,latency,models](
+        std::shared_ptr<HttpServer::Response> response,
+        std::shared_ptr<HttpServer::Request> request) {
+          try {
+            ptree pt;
+            read_json(request->content, pt);
 
-                    long uid = pt.get<long>("uid");
-                    std::shared_ptr<Input> input = decode_input(input_type, pt);
-                    auto prediction = q.predict(
-                        {name, uid, input, latency, policy, models});
-                    prediction.then([response](boost::future<Response> f){
-                        Response r = f.get();
-                        std::stringstream ss;
-                        ss << "qid:" << r.query_id_ << " predict:" << r.output_->y_hat_;
-                        std::string content = ss.str();
-                        *response << "HTTP/1.1 200 OK\r\nContent-Length: " << content.length() << "\r\n\r\n" << content;
-                    });
-                } catch (const ptree_error &e) {
-                    *response << "HTTP/1.1 200 OK\r\nContent-Length: " << std::strlen(e.what()) << "\r\n\r\n" << e.what() << "\n";
-                } catch (const std::invalid_argument &e) {
-                    *response << "HTTP/1.1 200 OK\r\nContent-Length: " << std::strlen(e.what()) << "\r\n\r\n" << e.what() << "\n";
-                }
-            };
-            std::string predict_endpoint = "^/" + name + "/predict$";
-            server.add_endpoint(predict_endpoint, "POST", predict_fn);
-            std::cout << "added endpoint (" + predict_endpoint + ") for " + name + "\n";
-
-            auto update_fn = [&q,name,input_type,output_type,policy,latency,models](std::shared_ptr<HttpServer::Response> response,
-                                                                                    std::shared_ptr<HttpServer::Request> request) {
-                try {
-                    ptree pt;
-                    read_json(request->content, pt);
-
-                    long uid = pt.get<long>("uid");
-                    std::shared_ptr<Input> input = decode_input(input_type, pt);
-                    std::shared_ptr<Output> output = decode_output(output_type, pt);
-                    auto update = q.update(
-                        {name, uid, {std::make_pair(input, output)}, policy, models});
-                    update.then([response](boost::future<FeedbackAck> f){
-                        FeedbackAck ack = f.get();
-                        std::stringstream ss;
-                        ss << "Feedback received? " << ack;
-                        std::string content = ss.str();
-                        *response << "HTTP/1.1 200 OK\r\nContent-Length: " << content.length() << "\r\n\r\n" << content;
-                    });
-                } catch (const ptree_error &e) {
-                    *response << "HTTP/1.1 200 OK\r\nContent-Length: " << std::strlen(e.what()) << "\r\n\r\n" << e.what() << "\n";
-                } catch (const std::invalid_argument &e) {
-                    *response << "HTTP/1.1 200 OK\r\nContent-Length: " << std::strlen(e.what()) << "\r\n\r\n" << e.what() << "\n";
-                }
-            };
-            std::string update_endpoint = "^/" + name + "/update$";
-            server.add_endpoint(update_endpoint, "POST", update_fn);
-            std::cout << "added endpoint (" + update_endpoint + ") for " + name + "\n";
-        }
-
-        void add_endpoint(std::string endpoint, std::string request_method,
-                          std::function<void(std::shared_ptr<HttpServer::Response>,
-                                             std::shared_ptr<HttpServer::Request>)> endpoint_fn) {
-            server.resource[endpoint][request_method] = endpoint_fn;
-            std::cout << "added " + endpoint + "\n";
-        }
-
-        void start_listening() {
-            HttpServer& s = server;
-            std::thread server_thread([&s](){
-                s.start();
+            long uid = pt.get<long>("uid");
+            std::shared_ptr<Input> input = decode_input(input_type, pt);
+            auto prediction =
+              q.predict({name,
+                         uid,
+                         input,
+                         latency,
+                         policy,
+                         models});
+            prediction.then([response](boost::future<Response> f){
+              Response r = f.get();
+              std::stringstream ss;
+              ss << "qid:" << r.query_id_ << " predict:" << r.output_.y_hat_;
+              std::string content = ss.str();
+              *response << "HTTP/1.1 200 OK\r\nContent-Length: "
+                        << content.length() << "\r\n\r\n"
+                        << content << "\n";
             });
+          } catch (const ptree_error &e) {
+            *response << "HTTP/1.1 200 OK\r\nContent-Length: "
+                      << std::strlen(e.what()) << "\r\n\r\n"
+                      << e.what() << "\n";
+          } catch (const std::invalid_argument &e) {
+            *response << "HTTP/1.1 200 OK\r\nContent-Length: "
+                      << std::strlen(e.what()) << "\r\n\r\n"
+                      << e.what() << "\n";
+          }
+      };
+      std::string predict_endpoint = "^/" + name + "/predict$";
+      server.add_endpoint(predict_endpoint, "POST", predict_fn);
+      std::cout << "added endpoint " + predict_endpoint + " for " + name + "\n";
 
-            server_thread.join();
-        }
-    private:
-        HttpServer server;
-        QueryProcessor& qp;
-};
+      auto update_fn = [&q,name,input_type,output_type,policy,latency,models](
+        std::shared_ptr<HttpServer::Response> response,
+        std::shared_ptr<HttpServer::Request> request) {
+          try {
+            ptree pt;
+            read_json(request->content, pt);
 
-/* Assume an input json of the following form:
- * {"name": <new endpoint name>, "models": <array of {"<model_name>": <model_version>} pairs>,
- *  "input_type": <input type string>, "output_type": <output type string>,
- *  "policy": <policy name>, "latency": <latency SLO>}
- */
-/*
-void application_endpoint(std::shared_ptr<HttpServer::Response> response,
-                          std::shared_ptr<HttpServer::Request> request,
-                          RequestHandler& rh) {
-    try {
-        ptree pt;
-        read_json(request->content, pt);
-
-        std::string name = pt.get<std::string>("name");
-        std::vector<VersionedModelId> models = parse_model_json(pt, "models");
-        std::string policy = pt.get<std::string>("policy");
-        long latency = pt.get<long>("latency");
-        std::string input_type_name = pt.get<std::string>("input_type");
-        std::string output_type_name = pt.get<std::string>("output_type");
-        InputType input_type;
-        OutputType output_type = output_type;
-        if (!input_type_name.compare("double_vec")) {
-            input_type = double_vec;
-        } else {
-            throw std::invalid_argument(input_type_name + " is not a valid input type");
-        }
-
-        if (!output_type_name.compare("double_val")) {
-            output_type = double_val;
-        } else {
-            throw std::invalid_argument(output_type_name + " is not a valid output type");
-        }
-
-        rh.add_application(name, models, input_type, output_type, policy, latency);
-        std::string response_string = "Successfully added endpoint: " + name + "\n";
-        *response << "HTTP/1.1 200 OK\r\nContent-Length: " << response_string.length() << "\r\n\r\n" << response_string;
-    } catch (const ptree_error &e) {
-        *response << "HTTP/1.1 200 OK\r\nContent-Length: " << std::strlen(e.what()) << "\r\n\r\n" << e.what() << "\n";
-    } catch (const std::invalid_argument &e) {
-        *response << "HTTP/1.1 200 OK\r\nContent-Length: " << std::strlen(e.what()) << "\r\n\r\n" << e.what() << "\n";
+            long uid = pt.get<long>("uid");
+            std::shared_ptr<Input> input = decode_input(input_type, pt);
+            Output output = decode_output(output_type, pt);
+            auto update =
+              q.update({name,
+                        uid,
+                        {std::make_pair(input, output)},
+                        policy,
+                        models});
+            update.then([response](boost::future<FeedbackAck> f){
+              FeedbackAck ack = f.get();
+              std::stringstream ss;
+              ss << "Feedback received? " << ack;
+              std::string content = ss.str();
+              *response << "HTTP/1.1 200 OK\r\nContent-Length: "
+                        << content.length() << "\r\n\r\n"
+                        << content << "\n";
+            });
+          } catch (const ptree_error &e) {
+            *response << "HTTP/1.1 200 OK\r\nContent-Length: "
+                      << std::strlen(e.what()) << "\r\n\r\n"
+                      << e.what() << "\n";
+          } catch (const std::invalid_argument &e) {
+            *response << "HTTP/1.1 200 OK\r\nContent-Length: "
+                      << std::strlen(e.what()) << "\r\n\r\n"
+                      << e.what() << "\n";
+          }
+      };
+      std::string update_endpoint = "^/" + name + "/update$";
+      server.add_endpoint(update_endpoint, "POST", update_fn);
+      std::cout << "added endpoint " + update_endpoint + " for " + name + "\n";
     }
-} */
+
+    void add_endpoint(
+        std::string endpoint,
+        std::string request_method,
+        std::function<void(std::shared_ptr<HttpServer::Response>,
+                           std::shared_ptr<HttpServer::Request>)>
+            endpoint_fn) {
+      server.resource[endpoint][request_method] = endpoint_fn;
+      std::cout << "added " + endpoint + "\n";
+    }
+
+    void start_listening() {
+      HttpServer& s = server;
+      std::thread server_thread([&s](){ s.start(); });
+
+      server_thread.join();
+    }
+  private:
+    HttpServer server;
+    QueryProcessor& qp;
+};
 
 int main() {
   QueryProcessor qp;
   RequestHandler rh(qp, "0.0.0.0", 1337, 1);
-
-  /* Add function for admin */
-  // auto application_fn = [&rh](std::shared_ptr<HttpServer::Response> response,
-  //                             std::shared_ptr<HttpServer::Request> request) {
-  //   application_endpoint(response, request, rh);
-  // };
-  // rh.add_endpoint("^/admin$", "POST", application_fn);
   rh.start_listening();
 }
