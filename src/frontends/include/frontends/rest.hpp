ea2f7a5d0a1538e409b73db3cb9bd2fdf442451a
Giulio Zhou <giuliozhou8@gmail.com>
2016-12-07 18:20:25 -0800

    Split rest frontend server into header and cpp

---
+++ src/frontends/include/frontends/rest.hpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <clipper/datatypes.hpp>
+#include <clipper/query_processor.hpp>
+
+#include <server_http.hpp>
+
+using clipper::FeedbackAck;
+using clipper::QueryProcessor;
+using clipper::VersionedModelId;
+using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
+
+enum InputType { integer_vec, double_vec, byte_vec, float_vec };
+enum OutputType { double_val, int_val };
+
+class RequestHandler {
+ public:
+  RequestHandler(QueryProcessor& q, int portno, int num_threads)
+      : server(portno, num_threads), qp(q) {}
+  RequestHandler(QueryProcessor& q, std::string address, int portno,
+                 int num_threads)
+      : server(address, portno, num_threads), qp(q) {}
+
+  void add_application(std::string name, std::vector<VersionedModelId> models,
+                       InputType input_type, OutputType output_type,
+                       std::string policy, long latency);
+  void add_endpoint(std::string endpoint, std::string request_method,
+                    std::function<void(std::shared_ptr<HttpServer::Response>,
+                                       std::shared_ptr<HttpServer::Request>)>
+                        endpoint_fn);
+  void start_listening();
+
+ private:
+  HttpServer server;
+  QueryProcessor& qp;
+};
--- src/frontends/src/rest.cpp
+++ src/frontends/src/rest.cpp
@@ -11,6 +11,7 @@
 
 #include <clipper/datatypes.hpp>
 #include <clipper/query_processor.hpp>
+#include <frontends/rest.hpp>
 
 #include <server_http.hpp>
 
@@ -23,9 +24,6 @@ using clipper::QueryProcessor;
 using clipper::Response;
 using clipper::VersionedModelId;
 using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
-
-enum InputType { integer_vec, double_vec, byte_vec, float_vec };
-enum OutputType { double_val, int_val };
 
 template <typename T>
 std::vector<T> as_vector(ptree const& pt, ptree::key_type const& key) {
@@ -60,107 +58,94 @@ Output decode_output(OutputType output_type, ptree parsed_json) {
   }
 }
 
-class RequestHandler {
- public:
-  RequestHandler(QueryProcessor& q, int portno, int num_threads)
-      : server(portno, num_threads), qp(q) {}
-  RequestHandler(QueryProcessor& q, std::string address, int portno,
-                 int num_threads)
-      : server(address, portno, num_threads), qp(q) {}
-
-  void add_application(std::string name, std::vector<VersionedModelId> models,
+void RequestHandler::add_application(std::string name, std::vector<VersionedModelId> models,
                        InputType input_type, OutputType output_type,
                        std::string policy, long latency) {
-    QueryProcessor& q = qp;
-    auto predict_fn = [&q, name, input_type, policy, latency, models](
-        std::shared_ptr<HttpServer::Response> response,
-        std::shared_ptr<HttpServer::Request> request) {
-      try {
-        ptree pt;
-        read_json(request->content, pt);
+  QueryProcessor& q = qp;
+  auto predict_fn = [&q, name, input_type, policy, latency, models](
+      std::shared_ptr<HttpServer::Response> response,
+      std::shared_ptr<HttpServer::Request> request) {
+    try {
+      ptree pt;
+      read_json(request->content, pt);
 
-        long uid = pt.get<long>("uid");
-        std::shared_ptr<Input> input = decode_input(input_type, pt);
-        auto prediction =
-            q.predict({name, uid, input, latency, policy, models});
-        prediction.then([response](boost::future<Response> f) {
-          Response r = f.get();
-          std::stringstream ss;
-          ss << "qid:" << r.query_id_ << " predict:" << r.output_.y_hat_;
-          std::string content = ss.str();
-          *response << "HTTP/1.1 200 OK\r\nContent-Length: " << content.length()
-                    << "\r\n\r\n"
-                    << content << "\n";
-        });
-      } catch (const ptree_error& e) {
-        *response << "HTTP/1.1 200 OK\r\nContent-Length: "
-                  << std::strlen(e.what()) << "\r\n\r\n"
-                  << e.what() << "\n";
-      } catch (const std::invalid_argument& e) {
-        *response << "HTTP/1.1 200 OK\r\nContent-Length: "
-                  << std::strlen(e.what()) << "\r\n\r\n"
-                  << e.what() << "\n";
-      }
-    };
-    std::string predict_endpoint = "^/" + name + "/predict$";
-    server.add_endpoint(predict_endpoint, "POST", predict_fn);
-    std::cout << "added endpoint " + predict_endpoint + " for " + name + "\n";
+      long uid = pt.get<long>("uid");
+      std::shared_ptr<Input> input = decode_input(input_type, pt);
+      auto prediction =
+          q.predict({name, uid, input, latency, policy, models});
+      prediction.then([response](boost::future<Response> f) {
+        Response r = f.get();
+        std::stringstream ss;
+        ss << "qid:" << r.query_id_ << " predict:" << r.output_.y_hat_;
+        std::string content = ss.str();
+        *response << "HTTP/1.1 200 OK\r\nContent-Length: " << content.length()
+                  << "\r\n\r\n"
+                  << content << "\n";
+      });
+    } catch (const ptree_error& e) {
+      *response << "HTTP/1.1 200 OK\r\nContent-Length: "
+                << std::strlen(e.what()) << "\r\n\r\n"
+                << e.what() << "\n";
+    } catch (const std::invalid_argument& e) {
+      *response << "HTTP/1.1 200 OK\r\nContent-Length: "
+                << std::strlen(e.what()) << "\r\n\r\n"
+                << e.what() << "\n";
+    }
+  };
+  std::string predict_endpoint = "^/" + name + "/predict$";
+  server.add_endpoint(predict_endpoint, "POST", predict_fn);
+  std::cout << "added endpoint " + predict_endpoint + " for " + name + "\n";
 
-    auto update_fn = [&q, name, input_type, output_type, policy, latency,
-                      models](std::shared_ptr<HttpServer::Response> response,
-                              std::shared_ptr<HttpServer::Request> request) {
-      try {
-        ptree pt;
-        read_json(request->content, pt);
+  auto update_fn = [&q, name, input_type, output_type, policy, latency,
+                    models](std::shared_ptr<HttpServer::Response> response,
+                            std::shared_ptr<HttpServer::Request> request) {
+    try {
+      ptree pt;
+      read_json(request->content, pt);
 
-        long uid = pt.get<long>("uid");
-        std::shared_ptr<Input> input = decode_input(input_type, pt);
-        Output output = decode_output(output_type, pt);
-        auto update = q.update(
-            {name, uid, {std::make_pair(input, output)}, policy, models});
-        update.then([response](boost::future<FeedbackAck> f) {
-          FeedbackAck ack = f.get();
-          std::stringstream ss;
-          ss << "Feedback received? " << ack;
-          std::string content = ss.str();
-          *response << "HTTP/1.1 200 OK\r\nContent-Length: " << content.length()
-                    << "\r\n\r\n"
-                    << content << "\n";
-        });
-      } catch (const ptree_error& e) {
-        *response << "HTTP/1.1 200 OK\r\nContent-Length: "
-                  << std::strlen(e.what()) << "\r\n\r\n"
-                  << e.what() << "\n";
-      } catch (const std::invalid_argument& e) {
-        *response << "HTTP/1.1 200 OK\r\nContent-Length: "
-                  << std::strlen(e.what()) << "\r\n\r\n"
-                  << e.what() << "\n";
-      }
-    };
-    std::string update_endpoint = "^/" + name + "/update$";
-    server.add_endpoint(update_endpoint, "POST", update_fn);
-    std::cout << "added endpoint " + update_endpoint + " for " + name + "\n";
-  }
+      long uid = pt.get<long>("uid");
+      std::shared_ptr<Input> input = decode_input(input_type, pt);
+      Output output = decode_output(output_type, pt);
+      auto update = q.update(
+          {name, uid, {std::make_pair(input, output)}, policy, models});
+      update.then([response](boost::future<FeedbackAck> f) {
+        FeedbackAck ack = f.get();
+        std::stringstream ss;
+        ss << "Feedback received? " << ack;
+        std::string content = ss.str();
+        *response << "HTTP/1.1 200 OK\r\nContent-Length: " << content.length()
+                  << "\r\n\r\n"
+                  << content << "\n";
+      });
+    } catch (const ptree_error& e) {
+      *response << "HTTP/1.1 200 OK\r\nContent-Length: "
+                << std::strlen(e.what()) << "\r\n\r\n"
+                << e.what() << "\n";
+    } catch (const std::invalid_argument& e) {
+      *response << "HTTP/1.1 200 OK\r\nContent-Length: "
+                << std::strlen(e.what()) << "\r\n\r\n"
+                << e.what() << "\n";
+    }
+  };
+  std::string update_endpoint = "^/" + name + "/update$";
+  server.add_endpoint(update_endpoint, "POST", update_fn);
+  std::cout << "added endpoint " + update_endpoint + " for " + name + "\n";
+}
 
-  void add_endpoint(std::string endpoint, std::string request_method,
-                    std::function<void(std::shared_ptr<HttpServer::Response>,
-                                       std::shared_ptr<HttpServer::Request>)>
-                        endpoint_fn) {
-    server.resource[endpoint][request_method] = endpoint_fn;
-    std::cout << "added " + endpoint + "\n";
-  }
+void RequestHandler::add_endpoint(std::string endpoint, std::string request_method,
+                  std::function<void(std::shared_ptr<HttpServer::Response>,
+                                     std::shared_ptr<HttpServer::Request>)>
+                      endpoint_fn) {
+  server.resource[endpoint][request_method] = endpoint_fn;
+  std::cout << "added " + endpoint + "\n";
+}
 
-  void start_listening() {
-    HttpServer& s = server;
-    std::thread server_thread([&s]() { s.start(); });
+void RequestHandler::start_listening() {
+  HttpServer& s = server;
+  std::thread server_thread([&s]() { s.start(); });
 
-    server_thread.join();
-  }
-
- private:
-  HttpServer server;
-  QueryProcessor& qp;
-};
+  server_thread.join();
+}
 
 int main() {
   QueryProcessor qp;
