e222d561deb5b40253e68cb49fe108c26c1ffe9b
Giulio Zhou <giuliozhou8@gmail.com>
2017-01-05 17:17:42 -0800

    SVM container for CIFAR10, with testing and data download scripts

---
+++ containers/python/download_and_extract_cifar10.py
@@ -0,0 +1,67 @@
+import cPickle
+import numpy as np
+import os
+import tarfile
+import urllib
+
+# Taken from https://www.cs.toronto.edu/~kriz/cifar.html
+def unpickle(file):
+    import cPickle
+    fo = open(file, 'rb')
+    dict = cPickle.load(fo)
+    fo.close()
+    return dict
+
+# download and extract the data
+if not os.path.exists('data'):
+    os.mkdir('data')
+if not os.path.exists('data/cifar-10-python.tar.gz'):
+    print("CIFAR10 dataset not found, downloading...")
+    # http://stackoverflow.com/a/19602990
+    testfile = urllib.URLopener()
+    testfile.retrieve('https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz',
+                      'data/cifar-10-python.tar.gz')
+    print("Finished!")
+
+print("Extracting tar file...")
+cifar10_tar = tarfile.open('data/cifar-10-python.tar.gz')
+cifar10_tar.extractall('data')
+cifar10_tar.close()
+
+print("Unpickling data batches...")
+batches_dir = 'data/cifar-10-batches-py'
+data, labels = [], []
+test_data, test_labels = [], []
+for filename in os.listdir(batches_dir):
+    file_path = os.path.join(batches_dir, filename)
+    if 'data_batch' in filename:
+        print(file_path)
+        batch_dict = unpickle(file_path)
+        data.append(batch_dict['data'])
+        labels.append(batch_dict['labels'])
+    elif filename == 'test_batch':
+        print(file_path)
+        batch_dict = unpickle(file_path)
+        test_data = batch_dict['data']
+        test_labels = np.array(batch_dict['labels'])
+        
+data = np.vstack(data)
+labels = np.hstack(labels)
+
+print("Writing out to CSV...")
+# Output to CSV format
+# Based off load_digits used in previous scripts
+# Format: label,pixel0,pixel1,...
+with open('data/train.data', 'w') as outfile:
+    for i in range(len(data)):
+        example_data = [labels[i]] + data[i].tolist()
+        example_data = map(lambda x: str(x), example_data)
+        output_line = ','.join(example_data) + '\n'
+        outfile.write(output_line)
+with open('data/test.data', 'w') as outfile:
+    for i in range(len(test_data)):
+        example_data = [test_labels[i]] + test_data[i].tolist()
+        example_data = map(lambda x: str(x), example_data)
+        output_line = ','.join(example_data) + '\n'
+        outfile.write(output_line)
+print("Finished!")
---
+++ containers/python/svm_container.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+import rpc
+import os
+import sys
+import numpy as np
+from sklearn.externals import joblib
+
+class SvmContainer(rpc.ModelContainerBase):
+    def __init__(self, path):
+        self.model = joblib.load(path)
+        print("Loaded %s model" % type(self.model), file=sys.stderr)
+        self.path = path
+
+    def predict_ints(self, inputs):
+        preds = self.model.predict(inputs)
+        # Change -1 to 0 for binary classification
+        preds[np.where(preds == -1)] = 0.
+        return preds
+
+    def predict_floats(self, inputs):
+        preds = self.model.predict(inputs)
+        # Change -1 to 0 for binary classification
+        preds[np.where(preds == -1)] = 0.
+        return preds
+
+
+if __name__ == "__main__":
+    try:
+        model_name = os.environ["CLIPPER_MODEL_NAME"]
+    except KeyError:
+        print("ERROR: CLIPPER_MODEL_NAME environment variable must be set", file=sys.stderr)
+        sys.exit(1)
+    try:
+        model_version = os.environ["CLIPPER_MODEL_VERSION"]
+    except KeyError:
+        print("ERROR: CLIPPER_MODEL_VERSION environment variable must be set", file=sys.stderr)
+        sys.exit(1)
+
+    ip = "127.0.0.1"
+    if "CLIPPER_IP" in os.environ:
+        ip = os.environ["CLIPPER_IP"]
+    else:
+        print("Connecting to Clipper on localhost")
+
+    port = 6001
+    if "CLIPPER_PORT" in os.environ:
+        port = int(os.environ["CLIPPER_PORT"])
+    else:
+        print("Connecting to Clipper with default port: 6001")
+
+    input_type = "doubles"
+    if "CLIPPER_INPUT_TYPE" in os.environ:
+        input_type = os.environ["CLIPPER_INPUT_TYPE"]
+    else:
+        print("Using default input type: doubles")
+
+    model_path = os.environ["CLIPPER_MODEL_PATH"]
+    pkl_names = [l for l in os.listdir(model_path) if os.path.splitext(l)[1] == ".pkl"]
+    assert len(pkl_names) == 1
+    pkl_path = os.path.join(model_path, pkl_names[0])
+    print(pkl_path, file=sys.stderr)
+    model = SvmContainer(pkl_path)
+    rpc.start(model, ip, port, model_name, model_version, input_type)
