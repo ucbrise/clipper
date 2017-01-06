1a6ac09149adb39d169b6a1d6ca527d13fd2735c
Corey Zumar <czumar@berkeley.edu>
2017-01-06 02:51:17 -0700

    More descriptive test for histogram percentile function, sort-based test

--- src/libclipper/test/metrics_test.cpp
+++ src/libclipper/test/metrics_test.cpp
@@ -67,30 +67,51 @@ TEST(MetricsTests, EWMACorrectness) {
   ASSERT_LE(meter.get_fifteen_minute_rate_seconds() - .07, .01);
 }
 
-TEST(MetricsTests, HistogramPercentile) {
+TEST(MetricsTests, HistogramPercentileFunctionCorrectness) {
   int64_t arr1[] = {15, 20, 35, 40, 50};
   double p1 = 0.4;
   double percentile1 = Histogram::percentile(std::vector<int64_t>(arr1, arr1 + 5), p1);
+  // The 40th percentile of the set of the data enumerated in arr1 should be 26
   ASSERT_EQ(static_cast<int>(percentile1), 26);
 
   std::vector<int64_t> vec2;
   vec2.push_back(15);
   double p2 = 0.4;
   double percentile2 = Histogram::percentile(vec2, p2);
+  // The 40th percentile of the set of data in vec2 should be 15
+  // because there's only one element
   ASSERT_EQ(static_cast<int>(percentile2), 15);
 
   double p3 = 0.0;
   double percentile3 = Histogram::percentile(vec2, p3);
+  // The 0th percentile of the set of data in vec2 should be 15
+  // because there's only one element
   ASSERT_EQ(static_cast<int>(percentile3), 15);
 
   double p4 = 1.0;
   double percentile4 = Histogram::percentile(vec2, p4);
+  // The 100th percentile of the set of data in vec2 should be 15
+  // because there's only one element
   ASSERT_EQ(static_cast<int>(percentile4), 15);
 
   std::vector<int64_t> vec3;
   // We expect a length error when computing a percentile
   // without snapshot data
   ASSERT_THROW(Histogram::percentile(vec3, p2), std::length_error);
+
+  int64_t arr2[] = {67, 31, 45, 40, 39};
+  double p5 = .6;
+  std::vector<int64_t> vec4;
+  std::vector<int64_t> vec5;
+  for(auto elem : arr2) {
+    vec4.push_back(elem);
+    vec5.push_back(elem);
+  }
+  std::sort(vec5.begin(), vec5.end());
+  // The 60th percentile of snapshots containing the same data
+  // in different orders should be the same, as the percentile
+  // function should correctly sort the data before calculation
+  ASSERT_EQ(Histogram::percentile(vec4, p5), Histogram::percentile(vec5, p5));
 }
 
 TEST(MetricsTests, HistogramStatsCorrectness) {
