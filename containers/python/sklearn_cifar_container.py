Minor fixes to normalization code for SVM Cifar10 example

containers/python/sklearn_cifar_container.py
@@ -12,7 +12,10 @@ class SklearnCifarContainer(rpc.ModelContainerBase):
         self.path = path
 
     def predict_floats(self, inputs):
-        preds = self.model.predict(inputs)
+        mean, sigma = np.mean(inputs, axis=1), np.std(inputs, axis=1)
+        np.place(sigma, sigma == 0, 1.)
+        normalized_inputs = np.transpose((inputs.T - mean) / sigma)
+        preds = self.model.predict(normalized_inputs)
         # Change -1 to 0 for binary classification
         preds[np.where(preds == -1)] = 0.
         return preds

examples/cifar_demo/train_sklearn_svm.py
@@ -9,7 +9,7 @@ classes = ['airplane', 'automobile', 'bird', 'cat',
 positive_class = classes.index('airplane')
 negative_class = classes.index('bird')
 
-def load_cifar(cifar_location, cifar_filename = "train.data", norm=True):
+def load_cifar(cifar_location, cifar_filename = "train.data", norm=False):
     cifar_path = cifar_location + "/" + cifar_filename
     print("Source file: %s" % cifar_path)
     df = pd.read_csv(cifar_path, sep=",", header=None)
@@ -19,9 +19,10 @@ def load_cifar(cifar_location, cifar_filename = "train.data", norm=True):
     X = data[:,1:]
     Z = X
     if norm:
-        mu = np.mean(X,0)
-        sigma = np.var(X,0)
-        Z = (X - mu) / np.array([np.sqrt(z) if z > 0 else 1. for z in sigma])
+        mu = np.mean(X.T,0)
+        sigma = np.var(X.T,0)
+        Z = (X.T - mu) / np.array([np.sqrt(z) if z > 0 else 1. for z in sigma])
+        Z = Z.T
     return (Z, y)
 
 def filter_data(X, y):
@@ -45,9 +46,9 @@ def train_svm(X, y):
     return model
 
 if __name__ == '__main__':
-    X, y = load_cifar('data')
+    X, y = load_cifar('data', norm=True)
     model = train_svm(X, y)
-    X_test, y_test = load_cifar('data', 'test.data')
+    X_test, y_test = load_cifar('data', 'test.data', norm=True)
     X_test, y_test = filter_data(X_test, y_test)
     print(model.score(X_test, y_test))
     if not os.path.exists('models'):