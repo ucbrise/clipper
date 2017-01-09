65375a93009d52ade6aba4039f55a861b2a4ec02
Dan Crankshaw <dscrankshaw@gmail.com>
2017-01-09 00:47:23 -0800

    updated docker files and added script to build all images

--- containers/python/build_container_images.sh
+++ containers/python/build_container_images.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
+
+
 # first build base image
 docker build -t clipper/py-rpc -f RPCDockerfile ./
 time docker build -t clipper/sklearn_cifar_container -f SklearnCifarDockerfile ./
-docker push clipper/sklearn_cifar_container
 time docker build -t clipper/tf_cifar_container -f TensorFlowCifarDockerfile ./
-docker push clipper/tf_cifar_container
 time docker build -t clipper/noop_container -f NoopDockerfile ./
 
