9fe7ba1e22563b6504f05495d9ccd6c697abf98c
Dan Crankshaw <dscrankshaw@gmail.com>
2017-03-05 15:26:21 -0800

    updated format script

--- bin/format.sh
+++ bin/format.sh
@@ -4,7 +4,22 @@ set -e
 set -u
 set -o pipefail
 
-pushd ..
-find ./src -not \( -path ./src/libs -prune \) -name '*pp' -print \
-    | xargs clang-format -style=file -i
-popd
+unset CDPATH
+# one-liner from http://stackoverflow.com/a/246128
+# Determines absolute path of the directory containing
+# the script.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Let the user start this script from anywhere in the filesystem.
+cd  $DIR/..
+
+num_violations="$(find ./src -not \( -path ./src/libs -prune \) -name '*pp' -print \
+    | xargs clang-format -style=file -output-replacements-xml \
+    | grep -c "<replacement ")"
+
+if [ $num_violations -eq 0 ]; then
+    exit 0
+else
+    echo "Found $num_violations Clang-Format violations"
+    exit 1
+fi
