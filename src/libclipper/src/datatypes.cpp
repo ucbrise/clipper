44b2cef0ba8a1fc42e645eef83f762e284f6b5f5
Corey Zumar <czumar@berkeley.edu>
2017-03-28 01:43:49 -0700

    Remove unnecessary spacing

--- src/libclipper/src/datatypes.cpp
+++ src/libclipper/src/datatypes.cpp
@@ -203,7 +203,6 @@ std::vector<ByteBuffer> rpc::PredictionRequest::serialize() {
   request_metadata.emplace_back(
       static_cast<uint32_t>(RequestType::PredictRequest));
 
-
   std::vector<uint32_t> input_metadata;
   input_metadata.emplace_back(static_cast<uint32_t>(input_type_));
   input_metadata.emplace_back(static_cast<uint32_t>(inputs_.size()));
@@ -227,8 +226,7 @@ std::vector<ByteBuffer> rpc::PredictionRequest::serialize() {
   ByteBuffer serialized_request_metadata = get_byte_buffer(request_metadata);
   ByteBuffer serialized_inputs =
       ByteBuffer(input_buf_start, input_buf_start + input_data_size_);
-
-
+  
   std::vector<long> input_metadata_size_bytes;
   // Add the size of the input metadata in bytes. This will be
   // sent prior to the input metadata to allow for proactive
