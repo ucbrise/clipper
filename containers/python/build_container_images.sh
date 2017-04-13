819e580aef57a86abadeced752f3c83010606b1c
Dan Crankshaw <dscrankshaw@gmail.com>
2017-04-13 09:42:34 -0700

    moved dockerfiles into same directory as build context

--- containers/python/build_container_images.sh
+++ containers/python/build_container_images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 # first build base image
-docker build -t clipper/py-rpc -f dockerfiles/RPCDockerfile ./
-time docker build -t clipper/noop-container -f dockerfiles/NoopDockerfile ./
-time docker build -t clipper/python-container -f dockerfiles/PythonContainerDockerfile ./
+docker build -t clipper/py-rpc -f RPCDockerfile ./
+time docker build -t clipper/noop-container -f NoopDockerfile ./
+time docker build -t clipper/python-container -f PythonContainerDockerfile ./
