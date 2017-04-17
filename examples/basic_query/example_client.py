3cca3eb3e26ee0c2e5837f73a3a48c9ac2c03990
Dan Crankshaw <dscrankshaw@gmail.com>
2017-04-17 11:50:38 -0700

    Fixed example client

--- examples/basic_query/example_client.py
+++ examples/basic_query/example_client.py
@@ -4,23 +4,7 @@ import requests
 from datetime import datetime
 import time
 import numpy as np
-
-
-def update(host, uid, x, y):
-    url = "http://%s:1337/example_app/update" % host
-    req_json = json.dumps({
-        'uid': uid,
-        'input': list(x),
-        'label': float(y),
-        'model_name': 'example_model',
-        'model_version': 1
-    })
-    headers = {'Content-type': 'application/json'}
-    start = datetime.now()
-    r = requests.post(url, headers=headers, data=req_json)
-    end = datetime.now()
-    latency = (end - start).total_seconds() * 1000.0
-    print("'%s', %f ms" % (r.text, latency))
+import sys
 
 
 def predict(host, uid, x):
@@ -34,37 +18,45 @@ def predict(host, uid, x):
     print("'%s', %f ms" % (r.text, latency))
 
 
-def add_mnist_app(host):
+def add_example_app(host):
     url = "http://%s:1338/admin/add_app" % host
     req_json = json.dumps({
-        "name":
-        "example_app",
-        "candidate_models": [{
-            "model_name": "example_model",
-            "model_version": 1
-        }],
-        "input_type":
-        "doubles",
-        "output_type":
-        "double",
-        "selection_policy":
-        "simple_policy",
-        "latency_slo_micros":
-        20000
+        "name": "example_app",
+        "candidate_model_names": ["example_model"],
+        "input_type": "doubles",
+        "default_output": -1.0,
+        "latency_slo_micros": 20000
     })
     headers = {'Content-type': 'application/json'}
-    start = datetime.now()
     r = requests.post(url, headers=headers, data=req_json)
-    end = datetime.now()
-    latency = (end - start).total_seconds() * 1000.0
-    print("'%s', %f ms" % (r.text, latency))
+    if r.status_code != requests.codes.ok:
+        print(r.text)
+        sys.exit(1)
+
+
+def add_example_model(host):
+    url = "http://%s:1338/admin/add_model" % host
+    req_json = json.dumps({
+        "model_name": "example_model",
+        "model_version": 1,
+        "labels": ["l1", "l2"],
+        "input_type": "doubles",
+        "container_name": "EXTERNAL",
+        "model_data_path": "EXTERNAL"
+    })
+    headers = {'Content-type': 'application/json'}
+    r = requests.post(url, headers=headers, data=req_json)
+    if r.status_code != requests.codes.ok:
+        print(r.text)
+        sys.exit(1)
 
 
 if __name__ == '__main__':
-    add_mnist_app("localhost")
+    host = "localhost"
+    add_example_app(host)
+    add_example_model(host)
     time.sleep(1.0)
-    uid = 4
+    uid = 0
     while True:
-        # mnist_update(uid, x[example_num], float(y[example_num]))
-        predict("localhost", uid, np.random.random(1000))
+        predict(host, uid, np.random.random(200))
         time.sleep(0.2)
