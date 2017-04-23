23ef839f80453f27714c5d79b4005195bc953bcc
Corey Zumar <czumar@berkeley.edu>
2017-04-23 04:40:39 -0700

    Fix test errors

--- src/benchmarks/src/rpc_protocol_test.cpp
+++ src/benchmarks/src/rpc_protocol_test.cpp
@@ -3,7 +3,6 @@
 
 #include <unordered_map>
 #include <mutex>
-#include <chrono>
 #include <vector>
 #include <sstream>
 
@@ -11,8 +10,9 @@
 #include <clipper/config.hpp>
 #include <clipper/constants.hpp>
 #include <clipper/redis.hpp>
+#include <clipper/datatypes.hpp>
 
-namespace clipper {
+using namespace clipper;
 
 const std::string LOGGING_TAG_RPC_TEST = "RPCTEST";
 
@@ -35,7 +35,6 @@ using RPCValidationResult = std::pair<bool, std::string>;
 class Tester {
  public:
   explicit Tester(const int num_containers) : rpc_(std::make_unique<rpc::RPCService>()),
-                                              msg_id_to_container_map(std::unordered_map<int, int>()),
                                               num_containers_(num_containers) {}
 
   Tester(const Tester &other) = delete;
@@ -45,25 +44,26 @@ class Tester {
   Tester &operator=(Tester &&other) = default;
   ~Tester() {
     std::unique_lock<std::mutex> l(test_completed_cv_mutex_);
-    bench_completed_cv_.wait(l, [this]() { return test_completed_ == true; });
+    test_completed_cv_.wait(l, [this]() { return test_completed_ == true; });
   }
 
   void start() {
-    rpc_.start("*",
+    rpc_->start("127.0.0.1",
                RPC_SERVICE_PORT,
-               [](VersionedModelId model, int container_id) {},
+               [](VersionedModelId /*model*/, int /*container_id*/) {},
                [this](rpc::RPCResponse response) {
                  on_response_received(response);
                });
+    Config& conf = get_config();
     while (!redis_connection_.connect(conf.get_redis_address(),
                                       conf.get_redis_port())) {
-      log_error(LOGGING_TAG_RPC_BENCH, "RPCBench failed to connect to redis",
+      log_error(LOGGING_TAG_RPC_TEST, "RPCTest failed to connect to redis",
                 "Retrying in 1 second...");
       std::this_thread::sleep_for(std::chrono::seconds(1));
     }
     while (!redis_subscriber_.connect(conf.get_redis_address(),
                                       conf.get_redis_port())) {
-      log_error(LOGGING_TAG_RPC_BENCH,
+      log_error(LOGGING_TAG_RPC_TEST,
                 "RPCBench subscriber failed to connect to redis",
                 "Retrying in 1 second...");
       std::this_thread::sleep_for(std::chrono::seconds(1));
@@ -75,14 +75,13 @@ class Tester {
         [this](const std::string &key, const std::string &event_type) {
           if (event_type == "hset") {
             // Detected a new container
-            long curr_time_millis = std::chrono::duration_cast<std::chrono::millis>(
-                std::chrono::system_clock::now().time_since_epoch()).count();
-            containers_connected += 1;
-            int container_id = container_info["zmq_connection_id"];
+            auto container_info =
+                redis::get_container_by_key(redis_connection_, key);
+            int container_id = std::stoi(container_info["zmq_connection_id"]);
             std::string model_name = container_info["model_name"];
             // Add a validation entry for the new connected container
             // indicating that it has not yet been validated
-            std::unique_lock lock(container_maps_mutex);
+            std::unique_lock<std::mutex> lock(container_maps_mutex);
             container_names_map_.emplace(container_id, model_name);
             const int validation_msg_id = send_validation_message(container_id);
             msg_id_to_container_map_.emplace(validation_msg_id, container_id);
@@ -90,9 +89,8 @@ class Tester {
         });
   }
 
-  const bool succeeded() {
-    std::unique_lock lock(container_maps_mutex);
-    int success_count = 0;
+  bool succeeded() {
+    std::unique_lock<std::mutex> lock(container_maps_mutex);
     for(auto const& container_entry : container_validation_map_) {
       bool rpc_valid = container_entry.second.first;
       if(!rpc_valid) {
@@ -102,10 +100,14 @@ class Tester {
     return true;
   }
 
-  const int send_validation_message(int container_id) {
-    std::vector<uint8_t> empty_msg;
-    int msg_id = rpc_->send_message(empty_msg, container_id);
-    std::unique_lock lock(container_maps_mutex);
+  int send_validation_message(int container_id) {
+    std::vector<double> data;
+    data.push_back(1);
+    std::shared_ptr<Input> input = std::make_shared<DoubleVector>(data);
+    rpc::PredictionRequest request(InputType::Doubles);
+    request.add_input(input);
+    auto serialized_request = request.serialize();
+    int msg_id = rpc_->send_message(serialized_request, container_id);
     return msg_id;
   }
 
@@ -135,7 +137,7 @@ class Tester {
 
   void on_response_received(rpc::RPCResponse response) {
     int msg_id = response.first;
-    std::unique_lock lock(container_maps_mutex);
+    std::unique_lock<std::mutex> lock(container_maps_mutex);
     auto container_id_entry = msg_id_to_container_map_.find(msg_id);
     if(container_id_entry == msg_id_to_container_map_.end()) {
       // TODO: Throw error
@@ -146,9 +148,14 @@ class Tester {
     if(container_valid_entry == container_validation_map_.end()) {
       // Container has not yet been validated
       std::vector<uint8_t> msg_history_bytes = response.second;
-      std::vector<int> parsed_message_history(
-          static_cast<int *>(msg_history_bytes.begin()), msg_history_bytes.size() / sizeof(int));
-      container_rpc_protocol_valid = validate_rpc_protocol(rpc_message_history);
+      size_t message_history_size = static_cast<size_t>(msg_history_bytes.size() / sizeof(int));
+      std::vector<int> parsed_message_history;
+      float* msg_history_floats = reinterpret_cast<float*>(msg_history_bytes.data());
+      for(int i = 0; static_cast<size_t>(i) < message_history_size; i++) {
+        int value = static_cast<int>(msg_history_floats[i]);
+        parsed_message_history.push_back(value);
+      }
+      container_rpc_protocol_valid = validate_rpc_protocol(parsed_message_history);
       container_validation_map_[container_id] = container_rpc_protocol_valid;
       containers_validated_ += 1;
       if(containers_validated_ == num_containers_) {
@@ -158,7 +165,7 @@ class Tester {
     } else {
       container_rpc_protocol_valid =
           RPCValidationResult(false, "Container sent excessive container content messages (expected 1)");
-      container_validation_map_[container_id] = failed_result;
+      container_validation_map_[container_id] = container_rpc_protocol_valid;
     }
     log_validation_result(container_id, container_rpc_protocol_valid);
   }
@@ -175,7 +182,7 @@ class Tester {
       return RPCValidationResult(false, "Initial protocol messages are of incorrect types!");
     }
     int received_container_content_count = 0;
-    for(int i = 3; i < rpc_message_history.size(); i++) {
+    for(int i = 3; i < static_cast<int>(rpc_message_history.size()); i++) {
       if(rpc_message_history[i] == MESSAGE_HISTORY_RECEIVED_CONTAINER_CONTENT) {
         received_container_content_count++;
       }
@@ -210,25 +217,26 @@ class Tester {
 
 int main(int argc, char *argv[]) {
   cxxopts::Options options("rpc_test", "Clipper RPC Correctness Test");
+  // clang-format off
   options.add_options()
       ("redis_ip", "Redis address",
        cxxopts::value<std::string>()->default_value("localhost"))
       ("redis_port", "Redis port",
        cxxopts::value<int>()->default_value("6379"))
       ("num_containers", "Number of containers to validate",
-       cxxopts::value<int>()->default_value(1));
+       cxxopts::value<int>()->default_value("1"));
+  // clang-format on
+  options.parse(argc, argv);
 
   get_config().set_redis_address(options["redis_ip"].as<std::string>());
   get_config().set_redis_port(options["redis_port"].as<int>());
+  get_config().ready();
 
   Tester tester(options["num_containers"].as<int>());
   tester.start();
 
   std::unique_lock<std::mutex> l(tester.test_completed_cv_mutex_);
   tester.test_completed_cv_.wait(
-      l, [&tester]() { return tester.test_completed_; });
+      l, [&tester]() { return tester.test_completed_ == true; });
   exit(tester.succeeded() ? EXIT_SUCCESS : EXIT_FAILURE);
-}
-
-
 }
