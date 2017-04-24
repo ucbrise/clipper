33fb322783dab3650fe2d90eee261383087151b6
Corey Zumar <czumar@berkeley.edu>
2017-04-24 02:54:12 -0700

    Update test for reconnection

--- containers/test/test_container_rpc.sh
+++ containers/test/test_container_rpc.sh
@@ -15,13 +15,20 @@ cd ../../
 ./configure && cd debug
 cd src/benchmarks
 make rpctest
+echo "Executing RPC test (first iteration)..."
 ./rpctest --num_containers=2 --timeout_seconds=20
-EXITCODE=$?
-if [ $EXITCODE == 0 ]
+EXITCODE_FIRST=$?
+echo "Sleeping for 5 seconds..."
+sleep 5s
+echo "Executing RPC test (second iteration)..."
+./rpctest --num_containers=2 --timeout_seconds=20
+EXITCODE_SECOND=$?
+if [ $EXITCODE_FIRST == 0 ] && [ $EXITCODE_SECOND == 0 ]
 then
    echo "TEST PASSED!"
+   exit 0
 else
    echo "TEST FAILED!"
+   exit 1
 fi
-exit $EXITCODE
 
