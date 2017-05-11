23b53de99bb9a89b75206d948c85e70c1ab7fe65
nishadsingh1 <nishadsingh@berkeley.edu>
2017-05-11 11:18:50 -0700

    [CLIPPER-162] [CLIPPER-163] [CLIPPER-146]: Check conda environment for issues in management library and fix bugs (#147)
    * Fixed bugs, added logging and documentation to python function deployment
    
    * Updated documentation with error debugging instructions
    
    * Check conda environment from Clipper management library before deploying python prediction function
    
    * addressed review comments
    
    * format code
    
    * More flush statements
    
    * format code

--- containers/python/rpc.py
+++ containers/python/rpc.py
@@ -171,6 +171,8 @@ class Server(threading.Thread):
         clipper_address = "tcp://{0}:{1}".format(self.clipper_ip,
                                                  self.clipper_port)
         poller = zmq.Poller()
+        sys.stdout.flush()
+        sys.stderr.flush()
         while True:
             socket = self.context.socket(zmq.DEALER)
             poller.register(socket, zmq.POLLIN)
@@ -189,6 +191,8 @@ class Server(threading.Thread):
                         if time_delta_millis >= SOCKET_ACTIVITY_TIMEOUT_MILLIS:
                             # Terminate the session
                             print("Connection timed out, reconnecting...")
+                            sys.stdout.flush()
+                            sys.stderr.flush()
                             connected = False
                             poller.unregister(socket)
                             socket.close()
@@ -287,6 +291,8 @@ class Server(threading.Thread):
                         print("recv: %f us, parse: %f us, handle: %f us" %
                               ((t2 - t1).microseconds, (t3 - t2).microseconds,
                                (t4 - t3).microseconds))
+                        sys.stdout.flush()
+                        sys.stderr.flush()
 
                     else:
                         feedback_request = FeedbackRequest(msg_id_bytes, [])
