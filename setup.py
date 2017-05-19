e1d90da80811a83c0a1d98f27e4d9919a0c80bc9
Nishad Singh <nishadsingh@berkeley.edu>
2017-05-19 13:27:16 -0700

    Setup for pip package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+  name = 'clipper_admin',
+  version = '0.1',
+  description = 'A low latency online prediction serving system',
+  author = 'Dan Crankshaw',
+  author_email = 'dcrankshaw@berkeley.edu',
+  url = 'https://github.com/ucbrise/clipper',
+  packages = ['clipper_admin'],
+  keywords = ['prediction', 'model', 'management'],
+  install_requires = ['requests', 'pyparsing', 'appdirs', 'pprint', 'subprocess32', 'sklearn', 'numpy', 'scipy', 'fabric', 'conda', 'pyyaml']
+)
