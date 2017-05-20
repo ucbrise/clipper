a18d01070f87906570fe71c3a3e1d8261c7da4b3
Nishad Singh <nishadsingh@berkeley.edu>
2017-05-19 17:11:35 -0700

    Optimisic startup implemented and reflected in docs

--- containers/python/python_container_entry.sh
+++ containers/python/python_container_entry.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
-conda install -y --file /model/conda_dependencies.txt
-pip install -r /model/pip_dependencies.txt
+IMPORT_ERROR_RETURN_CODE=3
+
+echo "Attempting to run Python container without installing dependencies"
 /bin/bash -c "exec python /container/python_container.py"
+if [ $? -eq $IMPORT_ERROR_RETURN_CODE ]; then
+	echo "Running Python container without installing dependencies fails"
+	echo "Will install dependencies and try again"
+  conda install -y --file /model/conda_dependencies.txt
+  pip install -r /model/pip_dependencies.txt
+  /bin/bash -c "exec python /container/python_container.py"
+fi
