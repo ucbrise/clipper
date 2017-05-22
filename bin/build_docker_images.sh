bb490849fa79121193fe40c9f9b2458df4b460ed
nishadsingh1 <nishadsingh@berkeley.edu>
2017-05-22 12:52:37 -0700

    [CLIPPER-154] Make a pip package (#165)
    * Setup for pip package
    
    * format code
    
    * Made requested changes
    
    * Fixup
    
    * Made requested changes -- added clipper_admin to requirements.txt and consolidated container build scripts
    
    * fixed formatting
    
    * Fixed import

--- bin/build_docker_images.sh
+++ bin/build_docker_images.sh
@@ -24,6 +24,12 @@ time docker build -t clipper/spark-scala-container -f SparkScalaContainerDockerf
 cd -
 
 # Build the Python model containers
-cd $DIR/../containers/python
-./build_container_images.sh
+cd $DIR/..
+
+# first build base image
+docker build -t clipper/py-rpc -f ./RPCDockerfile ./
+time docker build -t clipper/noop-container -f ./NoopDockerfile ./
+time docker build -t clipper/python-container -f ./PythonContainerDockerfile ./
+time docker build -t clipper/sklearn_cifar_container -f ./SklearnCifarDockerfile ./
+time docker build -t clipper/tf_cifar_container -f ./TensorFlowCifarDockerfile ./
 cd -
