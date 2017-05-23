21e5bce543bb3570131e6a09f7000423b67b9b6e
Dan Crankshaw <dscrankshaw@gmail.com>
2017-05-23 13:55:01 -0700

    remove conda from pip deps (#180)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='clipper_admin',
-    version='0.1.1',
+    version='0.1.2',
     description='Admin commands for the Clipper prediction-serving system',
     author='Dan Crankshaw',
     author_email='crankshaw@cs.berkeley.edu',
@@ -12,5 +12,5 @@ setup(
     keywords=['clipper', 'prediction', 'model', 'management'],
     install_requires=[
         'requests', 'pyparsing', 'appdirs', 'pprint', 'subprocess32',
-        'sklearn', 'numpy', 'scipy', 'fabric', 'conda', 'pyyaml'
+        'sklearn', 'numpy', 'scipy', 'fabric', 'pyyaml'
     ])
