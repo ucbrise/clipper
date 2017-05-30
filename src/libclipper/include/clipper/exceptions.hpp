ffb25a74fe8f149cdde8f41e766ff444097a23f0
Corey Zumar <czumar@berkeley.edu>
2017-05-30 03:47:50 -0600

    Revert "Remove msg_ from exception"
    This reverts commit 01ec13727eba105447d262b204c68c87a5c3a3a6.

--- src/libclipper/include/clipper/exceptions.hpp
+++ src/libclipper/include/clipper/exceptions.hpp
@@ -19,6 +19,9 @@ class NoModelsFoundError : public std::runtime_error {
  public:
   NoModelsFoundError();
   const char* what() const noexcept;
+
+ private:
+  std::string msg_;
 };
 
 }  // namespace clipper
