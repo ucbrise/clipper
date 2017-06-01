ec534f6cdab27a67e00ce13b6a8c3594ef8fd0cd
Dan Crankshaw <dscrankshaw@gmail.com>
2017-06-01 09:19:27 -0700

    fixed conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,8 @@ import sys
 import os
 import shlex
 
+import sphinx_rtd_theme
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -99,7 +101,9 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
