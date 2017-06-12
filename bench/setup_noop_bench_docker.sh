94f295e85f5331e97a05ed4ae46711d7a47c6f60
Ubuntu <ubuntu@ip-172-31-23-83.us-west-1.compute.internal>
2017-06-12 20:33:24 +0000

    Can now connect to model run in docker container

---
+++ bench/setup_noop_bench_docker.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+trap "exit" INT TERM
+trap "kill 0" EXIT
+
+export CLIPPER_MODEL_NAME="bench_noop"
+export CLIPPER_MODEL_VERSION="1"
+export CLIPPER_MODEL_PATH="model/"
+// Set this with curl http://169.254.169.254/latest/meta-data/local-ipv4 from the AWS instance
+export CLIPPER_IP="172.31.23.83"
+
+python /containers/python/noop_container.py 
