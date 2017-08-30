09170df895b666adc7a8de3255e674cc8cf49a5f
Dan Crankshaw <dscrankshaw@gmail.com>
2017-08-30 10:36:38 -0700

    added todo

--- bin/run_ci.sh
+++ bin/run_ci.sh
@@ -3,4 +3,5 @@
 set -e
 set -u
 
+# TODO: Remove the /tmp volume mount in the Clipper admin refactor PR
 docker run --rm --network=host -v /var/run/docker.sock:/var/run/docker.sock -v /tmp:/tmp clipper/unittests:develop
