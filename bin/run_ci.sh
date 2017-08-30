89283182e6e8d57394d470669bb0989c2cd830e3
Dan Crankshaw <dscrankshaw@gmail.com>
2017-08-30 14:29:17 -0700

    don't need to mount tmp in unit tests docker container anymore

--- bin/run_ci.sh
+++ bin/run_ci.sh
@@ -3,5 +3,4 @@
 set -e
 set -u
 
-# TODO: Remove the /tmp volume mount in the Clipper admin refactor PR
-docker run --rm --network=host -v /var/run/docker.sock:/var/run/docker.sock -v /tmp:/tmp clipper/unittests:develop
+docker run --rm --network=host -v /var/run/docker.sock:/var/run/docker.sock clipper/unittests:develop
