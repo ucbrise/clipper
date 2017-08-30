53258a6674a628f64dfb2b1471f6fd672d551449
Dan Crankshaw <dscrankshaw@gmail.com>
2017-08-30 09:17:09 +0000

    pretty sure dockerized tests are passing

---
+++ bin/ci_checks.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+unset CDPATH
+# one-liner from http://stackoverflow.com/a/246128
+# Determines absolute path of the directory containing
+# the script.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Let the user start this script from anywhere in the filesystem.
+$DIR/check_format.sh
+$DIR/run_unittests.sh
