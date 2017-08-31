b17a16d340586238551b6a1902fb051cc53503f2
Dan Crankshaw <dscrankshaw@gmail.com>
2017-08-31 03:30:19 -0700

    mount ~/.docker in unit tests docker file

--- bin/run_ci.sh
+++ bin/run_ci.sh
@@ -2,5 +2,18 @@
 
 set -e
 set -u
+set -o pipefail
 
-docker run --rm --network=host -v /var/run/docker.sock:/var/run/docker.sock -v /tmp:/tmp clipper/unittests:develop
+unset CDPATH
+# one-liner from http://stackoverflow.com/a/246128
+# Determines absolute path of the directory containing
+# the script.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+# Let the user start this script from anywhere in the filesystem.
+cd $DIR/..
+tag=$(<VERSION.txt)
+
+docker run --rm --network=host -v /var/run/docker.sock:/var/run/docker.sock -v /tmp:/tmp \
+    -v /home/jenkins/.docker:/root/.docker clipper/unittests:$tag
