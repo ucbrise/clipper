bc905d6e69e6fee7d6cbc7374b8e2e14c680d7b6
Dan Crankshaw <dscrankshaw@gmail.com>
2017-08-31 20:21:15 -0700

    try mocking out pyspark

--- clipper_admin/docs/conf.py
+++ clipper_admin/docs/conf.py
@@ -38,6 +38,16 @@ import shlex
 
 import sphinx_rtd_theme
 
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['pyspark']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
