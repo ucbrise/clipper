dded28400854ef0ec2644277e8b31998be5cb1bf
Dan Crankshaw <dscrankshaw@gmail.com>
2017-09-01 23:57:36 -0700

    revert version and add push to k8s testing cluster container registry

--- bin/run_ci.sh
+++ bin/run_ci.sh
@@ -18,6 +18,13 @@ tag=$(<VERSION.txt)
 # Build docker images
 ./bin/build_docker_images.sh
 
+# Tag and push the latest version of the Clipper Docker images to the container registry
+# for the Kubernetes testing cluster
+docker tag clipper/query_frontend:$tag 568959175238.dkr.ecr.us-west-1.amazonaws.com/clipper/query_frontend:$tag
+docker push 568959175238.dkr.ecr.us-west-1.amazonaws.com/clipper/query_frontend:$tag
+docker tag clipper/management_frontend:$tag 568959175238.dkr.ecr.us-west-1.amazonaws.com/clipper/management_frontend:$tag
+docker push 568959175238.dkr.ecr.us-west-1.amazonaws.com/clipper/management_frontend:$tag
+
 # Run tests
 docker run --rm --network=host -v /var/run/docker.sock:/var/run/docker.sock -v /tmp:/tmp \
     -v /home/jenkins/.docker:/root/.docker clipper/unittests:$tag
