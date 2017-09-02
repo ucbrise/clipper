bd80af20074ab4e724f829ab77710effb35c73ab
Dan Crankshaw <dscrankshaw@gmail.com>
2017-09-01 23:24:50 -0700

    Build script seems to be working

--- bin/build_docker_images.sh
+++ bin/build_docker_images.sh
@@ -60,31 +60,33 @@ build_images () {
     local private=false
     local public=true
 
-    ############## WARNING: ############
-    # Some of these images depend on each other prior images.
-    # You must preserve the build order.
-    create_image lib_base ClipperLibBaseDockerfile $private
-    create_image query_frontend QueryFrontendDockerfile $public
-    create_image management_frontend ManagementFrontendDockerfile $public
-    create_image unittests ClipperTestsDockerfile  $private
+    ########################$$####### WARNING: #################################
+    # Some of these images depend on each other prior images. Do not change
+    # the order in which the images are built
+    ###########################################################################
 
 
-    # TODO TODO TODO: fix this centralize
+    # create_image lib_base ClipperLibBaseDockerfile $private
+    # create_image query_frontend QueryFrontendDockerfile $public
+    # create_image management_frontend ManagementFrontendDockerfile $public
+    # create_image unittests ClipperTestsDockerfile  $private
+
+
     # Build Spark JVM Container
     # cd $DIR/../containers/jvm
     # time docker build -t clipper/spark-scala-container:$tag -f SparkScalaContainerDockerfile ./
     # cd -
 
-    # Build the Python model containers
-    cd $DIR/..
+    create_image spark-scala-container SparkScalaContainerDockerfile $public
 
+    # Build the Python model containers
     # first build base image
-    create_image py-rpc RPCDockerfile $public
-    create_image sum-container SumDockerfile  $private
-    create_image noop-container NoopDockerfile $public
-    create_image python-closure-container PyClosureContainerDockerfile $public
-    create_image pyspark-container PySparkContainerDockerfile $public
-    create_image tf_cifar_container TensorFlowCifarDockerfile $public
+    # create_image py-rpc RPCDockerfile $public
+    # create_image sum-container SumDockerfile  $private
+    # create_image noop-container NoopDockerfile $public
+    # create_image python-closure-container PyClosureContainerDockerfile $public
+    # create_image pyspark-container PySparkContainerDockerfile $public
+    # create_image tf_cifar_container TensorFlowCifarDockerfile $public
 }
 
 usage () {
