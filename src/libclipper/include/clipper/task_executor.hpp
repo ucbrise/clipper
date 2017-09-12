75fccb8b04f6c287a6ef4f354ab4818fdee63f46
Corey Zumar <czumar@berkeley.edu>
2017-09-12 16:57:53 -0700

    Format code

--- src/libclipper/include/clipper/task_executor.hpp
+++ src/libclipper/include/clipper/task_executor.hpp
@@ -498,7 +498,8 @@ class TaskExecutor {
       for (int batch_num = 0; batch_num < batch_size; ++batch_num) {
         InflightMessage completed_msg = keys[batch_num];
         cache_->put(completed_msg.model_, completed_msg.query_id_,
-                    Output{parsed_response.outputs_[batch_num], {completed_msg.model_}});
+                    Output{parsed_response.outputs_[batch_num],
+                           {completed_msg.model_}});
       }
     }
   }
