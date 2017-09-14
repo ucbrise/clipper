a634ae3723eab1ece5d95afa75adb281ae76d4a2
Dan Crankshaw <dscrankshaw@gmail.com>
2017-09-14 06:06:54 +0000

    Fixed unit tests

--- integration-tests/r_integration_test/deploy_query_test_model.py
+++ integration-tests/r_integration_test/deploy_query_test_model.py
@@ -6,6 +6,7 @@ import requests
 import json
 import logging
 import numpy as np
+import subprocess32 as subprocess
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.abspath("%s/../../clipper_admin" % cur_dir))
@@ -20,9 +21,9 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-APP_NAME = "rtest_app"
-APP_DEFAULT_VALUE = "default"
-APP_SLO = 10000000
+APP_NAME = "rtest-app"
+APP_DEFAULT_VALUE = "NONE"
+APP_SLO = 1000000
 
 INPUT_TYPE = "doubles"
 
@@ -31,17 +32,18 @@ MODEL_VERSION = 1
 MODEL_IMAGE_NAME = "rtest-model:1"
 
 
-def create_application(conn):
-    conn.register_application(APP_NAME, INPUT_TYPE, APP_DEFAULT_VALUE, APP_SLO)
+def create_application(clipper_conn):
+    clipper_conn.register_application(APP_NAME, INPUT_TYPE, APP_DEFAULT_VALUE, APP_SLO)
+    time.sleep(1)
 
 
-def deploy_and_link_model(conn):
-    conn.deploy_model(MODEL_NAME, MODEL_VERSION, INPUT_TYPE, MODEL_IMAGE_NAME)
-    conn.link_model_to_app(APP_NAME, MODEL_NAME)
+def deploy_and_link_model(clipper_conn):
+    subprocess.check_call(["Rscript", "build_test_model.R"])
+    clipper_conn.deploy_model(MODEL_NAME, MODEL_VERSION, INPUT_TYPE, MODEL_IMAGE_NAME)
+    clipper_conn.link_model_to_app(app_name=APP_NAME, model_name=MODEL_NAME)
 
 
-def send_requests(conn):
-
+def send_requests(clipper_conn):
     success = False
     num_tries = 0
 
@@ -49,21 +51,24 @@ def send_requests(conn):
         time.sleep(30)
         num_preds = 25
         num_success = 0
-        addr = conn.get_query_addr()
+        addr = clipper_conn.get_query_addr()
+        logger.info("ADDR: {}".format(addr))
         for i in range(num_preds):
             response = requests.post(
                 "http://%s/%s/predict" % (addr, APP_NAME),
                 headers=headers,
                 data=json.dumps({
-                    'input': list(np.random.random(i))
+                    'input': list(np.random.random(30))
                 }))
             result = response.json()
             if response.status_code == requests.codes.ok and not result["default"]:
                 num_success += 1
-            if num_success != num_preds > 0:
-                logger.error(
-                    "Error: %d/%d predictions were default or unsuccessful" %
-                    (num_preds - num_success, num_preds))
+            else:
+                logger.warning(result)
+        if num_success < num_preds:
+            logger.error(
+                "Error: %d/%d predictions were default or unsuccessful" %
+                (num_preds - num_success, num_preds))
         if num_success > num_preds / 2.0:
             success = True
         num_tries += 1
@@ -74,26 +79,16 @@ def send_requests(conn):
 
 if __name__ == "__main__":
 
-    conn = create_docker_connection(cleanup=True, start_clipper=True)
-
-    create_application(conn)
-    deploy_and_link_model(conn)
-
-    time.sleep(5)
-
-    send_requests(conn)
-    conn.stop_all()
-
     try:
         clipper_conn = create_docker_connection(
             cleanup=True, start_clipper=True)
         time.sleep(10)
         try:
             logger.info("Running R model deployment test")
-            create_application(conn)
-            deploy_and_link_model(conn)
+            create_application(clipper_conn)
+            deploy_and_link_model(clipper_conn)
             time.sleep(5)
-            send_requests(conn)
+            send_requests(clipper_conn)
             logger.info("R model deployment completed SUCCESSFULLY!")
         except BenchmarkException as e:
             logger.exception("BenchmarkException in R model deployment test")
--- integration-tests/r_integration_test/rclipper_test.sh
+++ integration-tests/r_integration_test/rclipper_test.sh
@@ -10,15 +10,10 @@ CLIPPER_ROOT=$DIR/../..
 USER_PACKAGE_DIR=$CLIPPER_ROOT/containers/R
 cd $USER_PACKAGE_DIR
 
-R -e "install.packages('versions', repos='http://cran.us.r-project.org')"
-R -e "versions::install.versions('CodeDepends', version='0.5-3')"
-
 R CMD INSTALL rclipper_user
 
 cd $DIR
 
 pip install -e $CLIPPER_ROOT/clipper_admin
-
-Rscript build_test_model.R
 
 python deploy_query_test_model.py
