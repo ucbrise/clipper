5a7bda9953580ac02f1ee2891f49de92fa8d7429
Haofan Wang <frankmiracle@outlook.com>
2017-11-20 15:44:45 -0800

    Add files via upload

---
+++ containers/python/pytorch_container.py
@@ -0,0 +1,112 @@
+from __future__ import print_function
+import rpc
+import os
+import sys
+import json
+
+import numpy as np
+
+# sys.path.append(os.path.abspath("/lib/"))
+from clipper_admin.deployers import cloudpickle
+
+import torch
+import importlib
+
+IMPORT_ERROR_RETURN_CODE = 3
+
+
+def load_predict_func(file_path):
+    with open(file_path, 'r') as serialized_func_file:
+        return cloudpickle.load(serialized_func_file)
+
+##### Need write code to load pytorch model here
+def load_pytorch_model(model_path):
+    print(model_path)
+    model = torch.load(model_path)  
+    return model
+
+##### Need write code for pytorch container here
+class PyTorchContainer(rpc.ModelContainerBase):
+    def __init__(self, path, input_type):
+        self.input_type = rpc.string_to_input_type(input_type)
+        modules_folder_path = "{dir}/modules/".format(dir=path)
+        sys.path.append(os.path.abspath(modules_folder_path))
+        predict_fname = "func.pkl"
+        predict_path = "{dir}/{predict_fname}".format(
+            dir=path, predict_fname=predict_fname)
+        self.predict_func = load_predict_func(predict_path)
+        
+        torch_model_path = os.path.join(path, "pytorch_model.pkl")
+        self.model = load_pytorch_model(torch_model_path)
+
+    def predict_ints(self, inputs):
+        preds = self.predict_func(self.model, inputs)
+        return [str(p) for p in preds]
+
+    def predict_floats(self, inputs):
+        preds = self.predict_func(self.model, inputs)
+        return [str(p) for p in preds]
+
+    def predict_doubles(self, inputs):
+        preds = self.predict_func(inputs)
+        return [str(p) for p in preds]
+
+    def predict_bytes(self, inputs):
+        preds = self.predict_func(self.model, inputs)
+        return [str(p) for p in preds]
+
+    def predict_strings(self, inputs):
+        preds = self.predict_func(self.model, inputs)
+        return [str(p) for p in preds]
+
+
+if __name__ == "__main__":
+    print("Starting PyTorchContainer container")
+    try:
+        model_name = os.environ["CLIPPER_MODEL_NAME"]
+    except KeyError:
+        print(
+            "ERROR: CLIPPER_MODEL_NAME environment variable must be set",
+            file=sys.stdout)
+        sys.exit(1)
+    try:
+        model_version = os.environ["CLIPPER_MODEL_VERSION"]
+    except KeyError:
+        print(
+            "ERROR: CLIPPER_MODEL_VERSION environment variable must be set",
+            file=sys.stdout)
+        sys.exit(1)
+
+    ip = "127.0.0.1"
+    if "CLIPPER_IP" in os.environ:
+        ip = os.environ["CLIPPER_IP"]
+    else:
+        print("Connecting to Clipper on localhost")
+
+    port = 7000
+    if "CLIPPER_PORT" in os.environ:
+        port = int(os.environ["CLIPPER_PORT"])
+    else:
+        print("Connecting to Clipper with default port: {port}".format(
+            port=port))
+
+    input_type = "doubles"
+    if "CLIPPER_INPUT_TYPE" in os.environ:
+        input_type = os.environ["CLIPPER_INPUT_TYPE"]
+    else:
+        print("Using default input type: doubles")
+
+    model_path = os.environ["CLIPPER_MODEL_PATH"]
+    print(model_path)
+
+    print("Initializing Pytorch function container")
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+    try:
+        model = PyTorchContainer(model_path, input_type)
+        rpc_service = rpc.RPCService()
+        rpc_service.start(model, ip, port, model_name, model_version,
+                          input_type)
+    except ImportError:
+        sys.exit(IMPORT_ERROR_RETURN_CODE)
