cf849f9dcb8d8df3210607a800b15022534f9cbe
Haofan Wang <frankmiracle@outlook.com>
2017-11-30 00:23:31 -0800

    Update deploy_pytorch_models.py

--- integration-tests/deploy_pytorch_models.py
+++ integration-tests/deploy_pytorch_models.py
@@ -10,15 +10,23 @@ import logging
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 sys.path.insert(0, os.path.abspath('%s/util_direct_import/' % cur_dir))
+
 from util_package import mock_module_in_package as mmip
 import mock_module as mm
 
 import torch
+from torch.utils.data import DataLoader
+import torch.utils.data as data
+from PIL import Image
+from torch import nn, optim
+from torch.autograd import Variable
+from torchvision import transforms
 
 from test_utils import (create_docker_connection, BenchmarkException, headers,
                         log_clipper_state)
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.abspath("%s/../clipper_admin" % cur_dir))
+
 from clipper_admin.deployers.pytorch import deploy_pytorch_model, create_endpoint
 
 logging.basicConfig(
@@ -43,16 +51,18 @@ def objective(y, pos_label):
     else:
         return 0
 
-#################need change
-def parseData(line, obj, pos_label):
-    fields = line.strip().split(',')
-    return LabeledPoint(
-        obj(int(fields[0]), pos_label), normalize(np.array(fields[1:])))
 
-##################need change
-def predict(spark, model, xs):
-    return [str(model.predict(normalize(x))) for x in xs]
+def parseData(train_path, pos_label):
+    trainData = np.genfromtxt(train_path, delimiter=',', dtype=int)
+    records = trainData[:, 1:]
+    labels = trainData[:, :1]
+    transformedlabels = [objective(ele, pos_label) for ele in labels]
+    return (records, transformedlabels)
 
+
+def predict(model, xs):
+    preds = model(xs)
+    return [str(p) for p in preds]
 
 def deploy_and_test_model(clipper_conn,
                           model,
@@ -97,12 +107,74 @@ def test_model(clipper_conn, app, version):
         raise BenchmarkException("Error querying APP %s, MODEL %s:%d" %
                                  (app, model_name, version))
 
-#################need change
-def train_logistic_regression(trainRDD):
-    return LogisticRegressionWithSGD.train(trainRDD, iterations=10)
+# Define Logistic Regression model
+class Logstic_Regression(nn.Module):
+    def __init__(self, in_dim, n_class):
+        super(Logstic_Regression, self).__init__()
+        self.logstic = nn.Linear(in_dim, n_class)
+
+    def forward(self, x):
+        out = self.logstic(x)
+        return out
+
+
+def train_logistic_regression(model,train_loader):
+    model = model
+    use_gpu = torch.cuda.is_available()
+    if use_gpu:
+        model = model.cuda()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001)
+    for epoch in range(1000):
+        print('*' * 10)
+        print('epoch {}'.format(epoch + 1))
+        since = time.time()
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 1):
+            img, label = data
+            if use_gpu:
+                img = Variable(img).cuda()
+                label = Variable(label).cuda()
+            else:
+                img = Variable(img)
+                label = Variable(label)
+
+            #Forward propagation
+            out = model(img)
+            loss = criterion(out, label)
+            running_loss += loss.data[0] * label.size(0)
+            _, pred = torch.max(out, 1)
+
+            #Back propagation
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        model.eval()
+
+    return model
+
 
 def get_test_point():
     return [np.random.randint(255) for _ in range(784)]
+
+#Define a dataloader to read our data
+class myDatafolder(data.Dataset):
+    def __init__(self, data, label):
+        self.imgs = data
+        self.classes = label
+
+    def __getitem__(self, index):
+        img = self.imgs[index]
+        label = self.classes[index]
+        img = torch.Tensor(img)
+        return img, torch.Tensor(label)
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def getName(self):
+        return self.classes
 
 
 if __name__ == "__main__":
@@ -112,6 +184,8 @@ if __name__ == "__main__":
             cleanup=True, start_clipper=True)
 
         train_path = os.path.join(cur_dir, "data/train.data")
+        (x, y) = parseData(train_path, pos_label)
+        train_loader = myDatafolder(x,y)
 
         try:
             clipper_conn.register_application(app_name, "integers",
@@ -131,7 +205,10 @@ if __name__ == "__main__":
                 raise BenchmarkException("Error creating app %s" % app_name)
 
             version = 1
-            lr_model = train_logistic_regression(trainRDD)
+
+            model = Logstic_Regression(28*28,2)
+            lr_model = train_logistic_regression(model,train_loader)
+
             deploy_and_test_model(
                 clipper_conn,
                 lr_model,
