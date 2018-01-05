086e305d9ec3e27f32812c7a4d10940ffc5c7ee4
Simon Mo <xmo@berkeley.edu>
2018-01-05 13:54:42 -0600

    [Metric] Trigger another CI check
    Pretty sure it isn't the issue with my code. But it now appears
    common enough.

--- bin/run_unittests.sh
+++ bin/run_unittests.sh
@@ -143,7 +143,7 @@ function run_integration_tests {
   python ../integration-tests/kubernetes_integration_test.py
   python ../integration-tests/deploy_tensorflow_models.py
   ../integration-tests/r_integration_test/rclipper_test.sh
-  python ../integration-tests/clipper_metric.py
+  python ../integration-tests/clipper_metric.py 
 }
 
 function run_all_tests {
