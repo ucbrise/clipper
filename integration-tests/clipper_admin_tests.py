4f12dac24f7052d81932454b21db8bc7e1854d40
Corey Zumar <czumar@berkeley.edu>
2018-01-22 11:15:46 -0800

    Fix test format

--- integration-tests/clipper_admin_tests.py
+++ integration-tests/clipper_admin_tests.py
@@ -511,7 +511,7 @@ class ClipperManagerTestCaseLong(unittest.TestCase):
             if batch_size == fixed_batch_size:
                 num_max_batch_queries += 1
 
-        self.assertGreaterEqual(num_max_batch_queries, 
+        self.assertGreaterEqual(num_max_batch_queries,
                                 int(total_num_queries * .7))
 
 
