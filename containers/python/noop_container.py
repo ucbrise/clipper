354a869af3f684a90f8a8d99304a3791d977c381
Corey Zumar <czumar@berkeley.edu>
2018-01-29 14:28:32 -0800

    roll back noop container

--- containers/python/noop_container.py
+++ containers/python/noop_container.py
@@ -25,7 +25,7 @@ class NoopContainer(rpc.ModelContainerBase):
         return self._predict(inputs)
 
     def predict_strings(self, inputs):
-        return [str(len(inp)) for inp in inputs]
+        return self._predict(inputs)
 
 if __name__ == "__main__":
     try:
