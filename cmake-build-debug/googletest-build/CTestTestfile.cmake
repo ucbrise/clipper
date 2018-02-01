53128e2d49e00b6fc836895c6bfe2c3ac934762f
Ao Li <aoli1996@berkeley.edu>
2018-02-01 11:18:35 -0800

    removed_cmake_debug

--- cmake-build-debug/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tim/Documents/Clipper_proj/clipper/cmake-build-debug/googletest-src
-# Build directory: /Users/tim/Documents/Clipper_proj/clipper/cmake-build-debug/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/src/management/CMakeFiles/management_frontend.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for management_frontend.
-# This may be replaced when dependencies are built.
