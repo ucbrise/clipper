d9b4a29b58ff893744d06e629fb075353f50a34f
Ubuntu <ubuntu@ip-172-31-2-230.us-west-2.compute.internal>
2018-02-05 05:49:04 +0000

    fixed model loading

--- containers/python/mxnet_container.py
+++ containers/python/mxnet_container.py
@@ -22,18 +22,19 @@ def load_predict_func(file_path):
 # load mxnet model from serialized dir
 def load_mxnet_model(model_path):
     #mxnet_model = mx.mod.Module.load(prefix=model_path, epoch=1)
-    train_path = "../../integration-tests/data/train.data"
-    data_iter = mx.io.CSVIter(
-        data_csv=train_path, data_shape=(785, ), batch_size=1)
+    #cur_dir = os.path.dirname(os.path.abspath(__file__))
+    #home_dir = os.path.expanduser("~")
+    #train_path = os.path.join(home_dir, "clipper/integration-tests/data/train.data")
+    #train_path = "/home/ubuntu/clipper/integration-tests/data/train.data"
+    data_iter = mx.io.CSVIter(data_csv="train.data", data_shape=(785, ), batch_size=1)
 
-    sym, arg_params, aux_params = mx.model.load_checkpoint(
-        prefix=model_path, epoch=0)
+    sym, arg_params, aux_params = mx.model.load_checkpoint(prefix=model_path, epoch=0)
     mxnet_model = mx.mod.Module(symbol=sym)
-    mxnet_model.bind(
-        for_training=False,
-        data_shapes=[('data', (1, 3, 224, 224))])  #, data_iter.provide_label)
-    arg_params['layer21_label'] = mx.nd.array([0])
-    mxnet_model.set_params(arg_params, aux_params)
+    mxnet_model.fit(train_data=data_iter, arg_params=arg_params, aux_params=aux_params, num_epoch=0)
+
+    #mxnet_model = mx.mod.Module(symbol=sym)
+    #mxnet_model.bind(data_iter.provide_data, data_iter.provide_label)
+    #mxnet_model.set_params(arg_params, aux_params)
     #mxnet_model.fit(fitParams=new FitParams().setArgParams(arg_params).setAuxParams(aux_params).\
     #                setBeginEpoch(1))
     return mxnet_model
