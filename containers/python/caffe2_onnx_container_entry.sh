60be51e8ebaf59ca032830ab5d595988506e0f39
Haofan Wang <frankmiracle@outlook.com>
2018-02-07 16:40:26 +0800

    Update caffe2_onnx_container_entry.sh

--- containers/python/caffe2_onnx_container_entry.sh
+++ containers/python/caffe2_onnx_container_entry.sh
@@ -5,11 +5,11 @@ IMPORT_ERROR_RETURN_CODE=3
 echo "Attempting to run Caffe2 ONNX container without installing dependencies"
 echo "Contents of /model"
 ls /model/
-/bin/bash -c "exec python /container/caffe2_container.py"
+/bin/bash -c "exec python /container/caffe2_onnx_container.py"
 if [ $? -eq $IMPORT_ERROR_RETURN_CODE ]; then
 	echo "Running Caffe2 container without installing dependencies fails"
 	echo "Will install dependencies and try again"
   conda install -y --file /model/conda_dependencies.txt
   pip install -r /model/pip_dependencies.txt
-  /bin/bash -c "exec python /container/caffe2_container.py"
+  /bin/bash -c "exec python /container/caffe2_onnx_container.py"
 fi
