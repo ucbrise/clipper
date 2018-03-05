bbca90682fadb6661f9b5882ff31c1adc62ab25d
Corey Zumar <czumar@berkeley.edu>
2018-03-05 15:00:14 -0800

    New serialization tests

--- containers/R/tests/run_tests.sh
+++ containers/R/tests/run_tests.sh
@@ -8,7 +8,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
 
-Rscript test_recursive_serialization.R
-Rscript test_obj_dependent_serialization.R
-Rscript test_lib_dependent_serialization.R
-Rscript test_file_dependent_serialization.R 
+# Rscript test_recursive_serialization.R
+# Rscript test_obj_dependent_serialization.R
+# Rscript test_lib_dependent_serialization.R
+Rscript test_file_dependent_serialization.R
+# Rscript test_multidependency_serialization.R
