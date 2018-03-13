71c348a2e8ef91e7e2c10ec8f8f2abcb659f1232
Dan Crankshaw <dscrankshaw@gmail.com>
2018-03-12 21:02:10 -0700

    added missing mxnet, pytorch, and pytorch-onnx-caffe2 integration tests

--- bin/run_unittests.sh
+++ bin/run_unittests.sh
@@ -142,6 +142,9 @@ function run_integration_tests {
   python ../integration-tests/deploy_pyspark_sparkml_models.py
   python ../integration-tests/kubernetes_integration_test.py
   python ../integration-tests/deploy_tensorflow_models.py
+  python ../integration-tests/deploy_mxnet_models.py 
+  python ../integration-tests/deploy_pytorch_models.py 
+  python ../integration-tests/deploy_pytorch_to_models.py 
   ../integration-tests/r_integration_test/rclipper_test.sh
   python ../integration-tests/clipper_metric_docker.py 
   python ../integration-tests/clipper_metric_kube.py 
