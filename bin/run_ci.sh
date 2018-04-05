249ea1ace11922d982961aa46f1fa5a6dc4e8c5c
Dan Crankshaw <dscrankshaw@gmail.com>
2018-04-05 16:35:14 +0000

    fixed py3 tests container name

--- bin/run_ci.sh
+++ bin/run_ci.sh
@@ -43,4 +43,4 @@ docker run --rm --network=host -v /var/run/docker.sock:/var/run/docker.sock -v /
     -e CLIPPER_K8S_PASSWORD=$CLIPPER_K8S_PASSWORD \
     -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
     -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-    clipper/py3tests:$tag
+    clipper/py35tests:$tag
