ab62e97c857ac5cb651d3eac72855afc87f16cec
Nikhil Duggirala <nikhild@berkeley.edu>
2018-04-13 17:19:32 -0700

    imported yaml

--- clipper_admin/clipper_admin/clipper_admin.py
+++ clipper_admin/clipper_admin/clipper_admin.py
@@ -14,6 +14,7 @@ import six
 from cloudpickle import CloudPickler
 import pickle
 import numpy as np
+import yaml
 
 from .container_manager import CONTAINERLESS_MODEL_IMAGE
 from .exceptions import ClipperException, UnconnectedException
