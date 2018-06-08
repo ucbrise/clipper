55c7d3f600ff40b98174a14232407b6315421120
Rehan Durrani <rdurrani@berkeley.edu>
2018-06-08 10:23:31 -0700

    Edit redox stub to be functioning.

--- selection_policy_testing/redox_stub.cpp
+++ selection_policy_testing/redox_stub.cpp
@@ -1,5 +1,4 @@
-/** This is the redox code we need to add to query_processing.cpp.
- Need to check if redox supports list actions. */
+/** This is the redox code we need to add to query_processing.cpp. */
 #include "redox.hpp"
 
 using namespace std;
@@ -11,17 +10,30 @@ int main() {
     if(!rdx.connect("localhost", 6379)) return 1;
     // The rest of the code is on trigger by recieving a query
     rdx.publish("query_rcvd", query_id);
-    auto& model_ids = rdx.commandSync<string>({"LRANGE 0 -1", query_id});
-    while(!model_ids.ok()) {
-        model_ids = rdx.commandSync<string>({"LRANGE 0 -1", query_id});
+    bool failed = true;
+    while failed {
+        rdx.command<vector<string>>({"LRANGE", query_id, "0", "-1"},
+                                    [](Command<vector<string>>& c){
+                                        if(c.ok()) {
+                                            failed = false;
+                                            for (const string& s : c.reply()) {
+                                                // Add model id to vector that stores models to send to
+                                            }
+                                        }
+                                    }
+                                    );
     }
     rdx.commandSync<string>({"DELETE", query_id});
     // Processing to get model ids from model_ids string
     // Code to pass query to appropriate containers
     // results = array of results from model containers
-    for (const string&result : results) {
-        rdx.commandSync<string>({"RPUSH", query_id, result}); // Need to check if redox supports this.
-    }
+    std::vector<string> cmd{"RPUSH", query_id};
+    cmd.insert(
+                cmd.end(),
+                std::make_move_iterator(results.begin()),
+                std::make_move_iterator(results.end())
+                );
+    rdx.commandSync<string>(cmd);
     rdx.publish("results_rcvd", query_id);
     auto& final_result_obj = rdx.commandSync<string>({"GET", std::to_string(query_id) + "_final"});
     while(!final_result_obj.ok()) {
