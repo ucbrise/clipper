31f275298d57a0ca223e6d87379fea7839652650
Simon Mo <xmo@berkeley.edu>
2018-12-27 23:36:10 -0600

    Optimizations in R container building; colorize output now clear escape char

--- bin/colorize_output.py
+++ bin/colorize_output.py
@@ -1,6 +1,7 @@
 from random import shuffle
 import sys
 import argparse
+import re
 
 parser = argparse.ArgumentParser(description="Colorize stdin; (optionally) add a tag.")
 parser.add_argument("--tag", type=str, help="Optional tag")
@@ -9,7 +10,7 @@ args = parser.parse_args()
 tag = "[{}]".format(args.tag) if args.tag else ""
 
 ALL_COLORS = [
-    # "\u001b[30m", # Black
+    "\u001b[30m", # Black
     "\u001b[31m",  # Red
     "\u001b[32m",  # Green
     "\u001b[33m",  # Yellow
@@ -19,15 +20,17 @@ ALL_COLORS = [
 ]
 
 shuffle(ALL_COLORS)
-
 COLOR = ALL_COLORS[0]
+
+# https://stackoverflow.com/questions/14693701/how-can-i-remove-the-ansi-escape-sequences-from-a-string-in-python
+ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
 
 for line in sys.stdin:
     print(
         "{begin_color} {tag} {line} {end_color}".format(
             begin_color=COLOR,
             tag=tag,
-            line=line.strip(),
+            line=ansi_escape.sub('', line.strip()),
             end_color="\u001b[0m",  # Reset
         )
     )
