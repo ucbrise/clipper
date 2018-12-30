b3f1cf69492ebc80da9a36c0d80a0c5d32828e22
Simon Mo <xmo@berkeley.edu>
2018-12-30 10:50:43 -0600

    Debug travis build

--- bin/run_travis.sh
+++ bin/run_travis.sh
@@ -15,6 +15,17 @@ make -j wait_for_kubernetes_test_containers
 
 # Run the following test in sequence
 cd integration-tests
+
+print_debug_info() {
+    while true; do
+        kubectl get pods | python ../bin/colorize_output.py --tag "kubectl pods"
+        kubectl describe pods | python ../bin/colorize_output.py --tag "kubectl describe"
+        sleep 10
+    done
+}
+
+print_debug_info &
+
 time python kubernetes_integration_test.py
 time python kubernetes_multi_frontend.py
 time python kubernetes_namespace.py
