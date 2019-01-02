0cb7d732471bb266ca56dcea11f2871077e6935a
Simon Mo <xmo@berkeley.edu>
2019-01-01 21:44:35 -0600

    [CI] Cleaning up #595 (#609)
    * fix cpu utilization issue
    
    * Apply withsmilo's patch
    
    * Add new timeout logic
    
    * Revert "Apply withsmilo's patch"
    
    This reverts commit 40ea77cf7b30a7fb08c9d0ea10d428b4a20dc241.
    
    * Fix travis
    
    * Fix bash equal
    
    * idk why sparkml failed, only reason is high cpu loads bumped it

--- bin/run_travis.sh
+++ bin/run_travis.sh
@@ -29,7 +29,7 @@ make -j -f CI_build.Makefile wait_for_kubernetes_test_containers
 cd integration-tests
 
 print_debug_info() {
-    kubectl get pods --all-namespace | python ../bin/colorize_output.py --tag "kubectl pods" --no-color
+    kubectl get pods --all-namespaces | python ../bin/colorize_output.py --tag "kubectl pods" --no-color
     kubectl describe pods | python ../bin/colorize_output.py --tag "kubectl describe" --no-color
 }
 
@@ -57,7 +57,7 @@ retry_test() {
             print_debug_info; 
             echo "failed at try $i, retrying"; 
             k_delete_all;
-            if [ "$i" -eq "2" ];  
+            if [ "$i" == "2" ];  
                 then 
                     print_debug_info
                     exit 1; 
