86400a612bd67aed6386f53c366baa82b0b2c0db
Zhang Tianhao <dumpkin1996@gmail.com>
2019-04-04 23:01:56 +0800

    Translation almost finished. Four containers included.

--- applications/translation/container4/app/predict.py
+++ applications/translation/container4/app/predict.py
@@ -7,8 +7,8 @@ Created on Wed Mar 20 12:10:10 2019
 """
 
 # Gensim
-import gensim
-from gensim.utils import simple_preprocess
+# import gensim
+# from gensim.utils import simple_preprocess
 # spacy for lemmatization
 import spacy
 import re
@@ -18,29 +18,25 @@ import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.ERROR)
 
 # NLTK Stop words
+import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 stop_words = stopwords.words('english')
 stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
-# python3 -m spacy download en
 nlp = spacy.load('en', disable=['parser', 'ner'])
 
 
 # tokenization
-def sent_to_words(sentences):
-    for sentence in sentences:
-        yield (gensim.utils.simple_preprocess(str(sentence), deacc=True))  # deacc=True removes punctuations
-
-
-# Define functions for stopwords and lemmatization
-def remove_stopwords(texts):
-    return [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+# def sent_to_words(sentences):
+#     for sentence in sentences:
+#         yield (gensim.utils.simple_preprocess(str(sentence), deacc=True))  # deacc=True removes punctuations
 
 
 def lemmatization(texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
     """https://spacy.io/api/annotation"""
     texts_out = []
     for sent in texts:
-        doc = nlp(" ".join(sent))
+        doc = nlp(" ".join([sent]))
         texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
     return texts_out
 
@@ -51,9 +47,9 @@ def text_clean(text):
     data = [re.sub('\s+', ' ', sent) for sent in data]
     # Remove distracting single quotes
     data = [re.sub("\'", "", sent) for sent in data]
-    data_words = list(sent_to_words(data))
+    data_words = [y for x in data for y in x.split(' ') ]
     # Remove Stop Words and lemmatizeation
-    data_words_nostops = remove_stopwords(data_words)
+    data_words_nostops = data_words
     data_lemmatized = lemmatization(data_words_nostops, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'])
     final = [word for word in data_lemmatized if word != []]
     data = ["".join(words) for words in final]
@@ -79,8 +75,9 @@ def predict(text_data):
 
     subject_analysis_result = "After cleaning: \n"
     subject_analysis_result += "Total number of meaningful words: " + str(count) + "\n"
-    subject_analysis_result += "Top three frequent words: (count, word)"
-    subject_analysis_result += word_freq[:3]
+    subject_analysis_result += "Top three frequent words: (count, word)\n"
+    for value, freq in word_freq[:3]:
+        subject_analysis_result += "The word " + str(freq) + " has appeared for " + str(value) + " times in this text\n"
 
     return subject_analysis_result
 
