dfc9d258e19f5ea1d7a10edbf9cf6418a7a849a6
simonz <zsxhku@gmail.com>
2019-04-10 15:52:48 +0800

    update

--- clipper_admin/clipper_admin/graph_parser.py
+++ clipper_admin/clipper_admin/graph_parser.py
@@ -29,6 +29,27 @@ def expand_dag(dag_description, container_info, proxy_info):
     new_list.append(list[0])
     new_list.append(list[1])
 
+    count = 0
+
     for node_info in list[2: 2+nodes_number]:
         
-        new_info = 
+        new_info = node_info \
+                    + '-' + container_info[count][0] \
+                    + '-' + container_info[count][1] \
+                    + '-' + container_info[count][2] \
+                    + '-' + proxy_info[count][0] \
+                    + '-' + proxy_info[count][1] \
+                    + '-' + proxy_info[count][2]
+
+        new_list.append(new_info)
+
+    for edge_info in list[2+nodes_number:]:
+        new_list.append(edge_info)
+
+    expanded_dag = ""
+
+    for info in new_list:
+        expanded_dag = expanded_dag + info + "\n"
+
+    return expanded_dag
+                    
