9606b1311f1cdc714e7538cc4dbbdd8b019e13bf
wall.kim <wall.kim@sk.com>
2019-04-26 10:40:16 +0900

    Modify get_string_array

--- src/libclipper/src/json_util.cpp
+++ src/libclipper/src/json_util.cpp
@@ -134,19 +134,12 @@ std::string get_string(rapidjson::Value& d, const char* key_name) {
   return std::string(v.GetString());
 }
 
-std::vector<std::string> get_string_array(rapidjson::Value& d,
-                                          const char* key_name) {
-  rapidjson::Value& v =
-      check_kv_type_and_return(d, key_name, rapidjson::kArrayType);
+std::vector<std::string> get_string_array(rapidjson::Value& d, const char* key_name) {
+  const Value& val =  d[key_name];
+  assert(val.IsArray());
   std::vector<std::string> vals;
-  vals.reserve(v.Capacity());
-  for (rapidjson::Value& elem : v.GetArray()) {
-    if (!elem.IsString()) {
-      throw json_semantic_error("Array input of type " +
-                                kTypeNames[elem.GetType()] +
-                                " is not of type string");
-    }
-    vals.push_back(elem.GetString());
+  for(SizeType i = 0; i < val.size(); i++){
+    vals.push_back(val[i].GetString());
   }
   return vals;
 }
