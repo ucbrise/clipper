4c235c145fe1220212b6c1e7a42716e816cd8c4c
Fred <yy0125@connect.hku.hk>
2019-05-11 16:55:38 +0800

    Correct ./ error

--- applications/imagequery/2_imageCaptionGenerator/wrapper_script.sh
+++ applications/imagequery/2_imageCaptionGenerator/wrapper_script.sh
@@ -10,7 +10,7 @@ apt-get install pkg-config zip zlib1g-dev unzip
 # 2. set access level to the installer
 chmod +x /container/bazel-0.25.1-installer-linux-x86_64.sh
 # 3. run bazel installer
-./container/bazel-0.25.1-installer-linux-x86_64.sh --user
+/container/bazel-0.25.1-installer-linux-x86_64.sh --user
 # 4. add to system path
 export PATH="$PATH:$HOME/bin"
 # Finish installing bazel 
@@ -20,8 +20,8 @@ chmod 777 /container/workspace/im2txt/runWithArg.sh
 
 # III. Download model from google drive
 # Reference: https://stackoverflow.com/questions/48133080/how-to-download-a-google-drive-url-via-curl-or-wget/48133859
-./container/workspace/im2txt/model/newDownload1.sh
-./container/workspace/im2txt/model/newDownload2.sh
+/container/workspace/im2txt/model/newDownload1.sh
+/container/workspace/im2txt/model/newDownload2.sh
 mv /newmodel.ckpt-2000000.meta /container/workspace/im2txt/model
 mv /newmodel.ckpt-2000000.data-00000-of-00001 /container/workspace/im2txt/model
 
