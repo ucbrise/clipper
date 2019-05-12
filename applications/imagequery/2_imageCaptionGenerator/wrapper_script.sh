08ed46b928785cbfb5eaa08e275ab07f0fad20fb
Oahzir <zsxhku@gmail.com>
2019-05-12 17:31:32 +0800

    update

--- applications/imagequery/2_imageCaptionGenerator/wrapper_script.sh
+++ applications/imagequery/2_imageCaptionGenerator/wrapper_script.sh
@@ -6,25 +6,26 @@
 
 # I. Installing bazel
 # 1. install required packages for installation
-apt-get install pkg-config zip zlib1g-dev unzip
+# apt-get install pkg-config zip zlib1g-dev unzip
 # 2. set access level to the installer
-chmod +x /container/bazel-0.25.1-installer-linux-x86_64.sh
+# chmod +x /container/bazel-0.25.1-installer-linux-x86_64.sh
 # 3. run bazel installer
-/container/bazel-0.25.1-installer-linux-x86_64.sh --user
+#/container/bazel-0.25.1-installer-linux-x86_64.sh --user
 # 4. add to system path
-export PATH="$PATH:$HOME/bin"
+#export PATH="$PATH:$HOME/bin"
 # Finish installing bazel 
 
 # II. Set access level to the shellscript to be executed with bazel
-chmod 777 /container/workspace/im2txt/runWithArg.sh
+# chmod 777 /container/workspace/im2txt/runWithArg.sh
 
 # III. Download model from google drive
 # Reference: https://stackoverflow.com/questions/48133080/how-to-download-a-google-drive-url-via-curl-or-wget/48133859
-/container/workspace/im2txt/model/newDownload1.sh # /notebooks/newmodel.ckpt-2000000.meta 
-/container/workspace/im2txt/model/newDownload2.sh # /notebooks/newmodel.ckpt-2000000.data-00000-of-00001
-mv /notebooks/newmodel.ckpt-2000000.meta /container/workspace/im2txt/model
-mv /notebooks/newmodel.ckpt-2000000.data-00000-of-00001 /container/workspace/im2txt/model
 
-echo "finished wrapper script"
+#/container/workspace/im2txt/model/newDownload1.sh # /notebooks/newmodel.ckpt-2000000.meta 
+#/container/workspace/im2txt/model/newDownload2.sh # /notebooks/newmodel.ckpt-2000000.data-00000-of-00001
+#mv /notebooks/newmodel.ckpt-2000000.meta /container/workspace/im2txt/model
+#mv /notebooks/newmodel.ckpt-2000000.data-00000-of-00001 /container/workspace/im2txt/model
+
+#echo "finished wrapper script"
 # run simlerpc server
 python /container/predict.py
