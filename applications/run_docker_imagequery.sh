d6597b1f738937d0d01bc18ea02b65f76b6a40b5
Fred <yy0125@connect.hku.hk>
2019-05-12 14:59:59 +0800

    try tf-gpu

--- applications/run_docker_imagequery.sh
+++ applications/run_docker_imagequery.sh
@@ -6,3 +6,9 @@ docker run -p 9000:9000 -it imagequery:container2  &
 docker run -p 11000:11000 -it imagequery:container3  &
 docker run -p 12000:12000 -it imagequery:container4  &
 
+
+# in detached mode
+docker run -d -p 8000:8000 imagequery:container1  &
+docker run -d -p 9000:9000 imagequery:container2  &
+docker run -d -p 11000:11000 imagequery:container3  &
+docker run -d -p 12000:12000 imagequery:container4  &
