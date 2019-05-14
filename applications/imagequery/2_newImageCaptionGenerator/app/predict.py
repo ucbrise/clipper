02ec1f319a223ff9863e748e0f89da9aa2d6ee9c
Fred <yy0125@connect.hku.hk>
2019-05-14 20:41:36 +0800

    update

--- applications/imagequery/2_newImageCaptionGenerator/app/predict.py
+++ applications/imagequery/2_newImageCaptionGenerator/app/predict.py
@@ -5,12 +5,12 @@ import json
 from timeit import default_timer as timer
 
 def generateCaption(image_name):
-  workspace_path = "/container/workspace/"
-  checkpoint_path = workspace_path + "im2txt/model/newmodel.ckpt-2000000"
-  wordscount_path = workspace_path + "im2txt/data/word_counts.txt"
-  image_path = workspace_path + "im2txt/data/images/" + image_name
-  command = "bash /container/workspace/im2txt/runWithArg.sh " + checkpoint_path + " " + wordscount_path + " " + image_path + " "
-  os.system(command)
+  # workspace_path = "/container/workspace/"
+  # checkpoint_path = workspace_path + "im2txt/model/newmodel.ckpt-2000000"
+  # wordscount_path = workspace_path + "im2txt/data/word_counts.txt"
+  # image_path = workspace_path + "im2txt/data/images/" + image_name
+  # command = "bash /container/workspace/im2txt/runWithArg.sh " + checkpoint_path + " " + wordscount_path + " " + image_path + " "
+  # os.system(command)
 
   # The caption data will be written to /container/captionData/captions.txt
   # we read the content of caption.txt in captionData and return it here
--- applications/imagequery/2_newImageCaptionGenerator/im2txt/runWithArg.sh
+++ applications/imagequery/2_newImageCaptionGenerator/im2txt/runWithArg.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # The path to the model and checkpoint
-CHECKPOINT_PATH=$1
+CHECKPOINT_PATH="/container/workspace/im2txt/model/newmodel.ckpt-2000000"
 # the path to the word_counts.txt
-VOCAB_FILE=$2
+VOCAB_FILE="/container/workspace/im2txt/data/word_counts.txt"
 # the image to be analyzed
-IMAGE_FILE=$3
-echo "CHECKPOINT_PATH: $1"
-echo "VOCAB_FILE: $2"
-echo "IMAGE_FILE: $3"
+IMAGE_FILE="/container/workspace/im2txt/data/images/image.jpg"
+echo "CHECKPOINT_PATH: ${CHECKPOINT_PATH}"
+echo "VOCAB_FILE: ${VOCAB_FILE}"
+echo "IMAGE_FILE: ${IMAGE_FILE}"
 
 python /container/workspace/im2txt/run_inference.py --checkpoint_path ${CHECKPOINT_PATH} --vocab_file ${VOCAB_FILE} --input_files ${IMAGE_FILE}
