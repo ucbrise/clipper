1bf0aa8254da30e0339930882012e01fbeb0feb1
Sungjun.Kim <sungjunkim@linecorp.com>
2019-05-15 06:45:15 +0900

    Change k8s ServiceTypes to be changeable (#667)
    * Change k8s ServiceTypes to be changeable
    
    When creating a KubernetesManager object, you could define own ServiceTypes. For example,
    cm = KubernetesContainerManager(
             ....,
             service_types={
                 'redis': 'NodePort',
                 'management': 'LoadBalancer',
                 'query': 'LoadBalancer',
                 'query-rpc': 'ClusterIP',
                 'metric': 'LoadBalancer'})
    
    * Fix error string
    
    * Fix wrong set operation
    
    * Change string format
    
    * Funtionize the codes to determine service_types and add test cases.
    
    * Fix bugs
    
    * Support service_types(LOAD_BALANCER) on connect()
    
    * Fix a bug
    
    * Write/load service_types to/from a yaml file
    
    * Fix a bug
    
    * Fix bug
    
    * Support multi-tenancy
    
    * Move the related test case to 'kubernetes_integration_test'
    
    * Use 'cluster_identifier' instead of 'cluster_name'
    
    * Revert clipper_admin_tests

--- integration-tests/test_utils.py
+++ integration-tests/test_utils.py
@@ -157,6 +157,7 @@ def create_kubernetes_connection(cleanup=False,
                                  cleanup_name='default-cluster',
                                  new_name='default-cluster',
                                  connect_name='default-cluster',
+                                 service_types=None,
                                  namespace='default'):
     logger.info("Creating KubernetesContainerManager")
     cl = None
@@ -172,6 +173,7 @@ def create_kubernetes_connection(cleanup=False,
         cm = KubernetesContainerManager(
             cluster_name=cleanup_name,
             useInternalIP=USE_MINIKUBE,
+            service_types=service_types,
             kubernetes_proxy_addr=kubernetes_proxy_addr)
         cl = ClipperConnection(cm)
         cl.stop_all()
@@ -184,6 +186,7 @@ def create_kubernetes_connection(cleanup=False,
             kubernetes_proxy_addr=kubernetes_proxy_addr,
             namespace=namespace,
             useInternalIP=USE_MINIKUBE,
+            service_types=service_types,
             create_namespace_if_not_exists=True)
         cl = ClipperConnection(cm)
         cl.start_clipper(num_frontend_replicas=num_frontend_replicas)
@@ -193,6 +196,7 @@ def create_kubernetes_connection(cleanup=False,
             cm = KubernetesContainerManager(
                 cluster_name=connect_name,
                 useInternalIP=USE_MINIKUBE,
+                service_types=service_types,
                 kubernetes_proxy_addr=kubernetes_proxy_addr)
             cl = ClipperConnection(cm)
             cl.connect()
