cd91f04ab931d95dada82bf68b3257812b2fdc47
Fred <yy0125@connect.hku.hk>
2019-05-14 20:04:11 +0800

    update

--- applications/imagequery/2_newImageCaptionGenerator/im2txt/runWithArg.sh
+++ applications/imagequery/2_newImageCaptionGenerator/im2txt/runWithArg.sh
@@ -9,15 +9,4 @@ echo "CHECKPOINT_PATH: $1"
 echo "VOCAB_FILE: $2"
 echo "IMAGE_FILE: $3"
 
-# bazel compilation
-# go to the workspace
-# cd /container/workspace/
-# //im2txt:run_inference means: go to my workspace, go to im2txt and execute run_inference
-# bazel build -c opt //im2txt:run_inference
-# compile bazel with parameters
-# bazel-bin/im2txt/run_inference \
-#   --checkpoint_path=${CHECKPOINT_PATH} \
-#   --vocab_file=${VOCAB_FILE} \
-#   --input_files=${IMAGE_FILE}
-
 python3 /container/im2txt/run_inference.py --checkpoint_path ${CHECKPOINT_PATH} --vocab_file ${VOCAB_FILE} --input_files ${IMAGE_FILE}
