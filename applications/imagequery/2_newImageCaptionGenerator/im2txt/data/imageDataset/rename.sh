70b1400c38719fda8a6a9762e2f35b3ef76a9d78
Fred <yy0125@connect.hku.hk>
2019-05-15 12:17:58 +0800

    a

--- applications/imagequery/2_newImageCaptionGenerator/im2txt/data/imageDataset/rename.sh
+++ applications/imagequery/2_newImageCaptionGenerator/im2txt/data/imageDataset/rename.sh
@@ -4,27 +4,25 @@ cd /container/workspace/im2txt/data/imageDataset/101_ObjectCategories
 
 # copy all images from subdirectories to current directory: imageDatasets
 directories="directoryList.txt"
-ls -d */
 ls -d */ > $directories
 while read directory; do
 	cp $directory/* .
 done < $directories
 
 # Reindex all jpg files in the current directory
-imageFiles="jpgFiles.txt"
-ls *.jpg
-ls *.jpg > $imageFiles
-let i=1
-while read imageFile; do
-	mv $imageFile "image$i.jpg"
-	let i=$i+1
-done < $imageFiles
+# imageFiles="jpgFiles.txt"
+# ls *.jpg > $imageFiles
+# let i=1
+# while read imageFile; do
+# 	mv $imageFile "image$i.jpg"
+# 	let i=$i+1
+# done < $imageFiles
 
 # remove temp files
-rm $directories
-rm $imageFiles
+# rm $directories
+# rm $imageFiles
 
 # for debug
 # ls /container/workspace/im2txt/data/imageDataset/101_ObjectCategories
-find -type f -name -name *.jpg | wc -l
-echo "$(find -type f -name -name *.jpg | wc -l) jpg images existing in this dataset."
+# echo "$(find -type f -name -name *.jpg | wc -l) jpg images existing in this dataset."
+ls *.jpg
