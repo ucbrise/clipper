6b856ce13f243c1605d19b838f9516631e00492a
Fred <yy0125@connect.hku.hk>
2019-05-18 13:55:05 +0800

    a

--- applications/imagequery/c3_nlpMappingGenerator/app/predict.py
+++ applications/imagequery/c3_nlpMappingGenerator/app/predict.py
@@ -2,7 +2,7 @@
 # Ref2: textacy official documentation for matching pattern
 import spacy
 import textacy
-from preprocess import preprocess
+from c3_nlpMappingGenerator.app.preprocess import preprocess
 from timeit import default_timer as timer
 
 # def print_document(doc):
