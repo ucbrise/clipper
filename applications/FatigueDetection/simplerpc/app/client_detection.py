4a21dee67f42a2d84b9bb84a0ca9e4052bfff597
cypressd1999 <49230186+cypressd1999@users.noreply.github.com>
2019-05-19 00:30:45 +0800

    Update client_detection.py

--- applications/FatigueDetection/simplerpc/app/client_detection.py
+++ applications/FatigueDetection/simplerpc/app/client_detection.py
@@ -31,28 +31,28 @@ def run():
         exit()
     
     #container2 is Drowsiness Detector, output a boolean value indicating whether the driver is sleeping or not, and memcached
-    container2 = xmlrpc.client.ServerProxy('http://0.0.0.0:9000')
-    drowsiness = container2.Predict(image_data)
-    print("\n[INFO] Drowsiness Detection FINISHED")
-    if drowsiness==True:
-        print("\n[PREDICTION] DROWSINESS ALERT!")
-    else:
-        print("\n[PREDICTION] No Drowsiness! Safe!")
+#     container2 = xmlrpc.client.ServerProxy('http://0.0.0.0:9000')
+#     drowsiness = container2.Predict(image_data)
+#     print("\n[INFO] Drowsiness Detection FINISHED")
+#     if drowsiness==True:
+#         print("\n[PREDICTION] DROWSINESS ALERT!")
+#     else:
+#         print("\n[PREDICTION] No Drowsiness! Safe!")
 
-    container3 = xmlrpc.client.ServerProxy('http://0.0.0.0:11000')
-    human_segmentation = container3.Predict(image_str)
-    print("\n[INFO] Human Extraction FINISHED")
-    if human_segmentation==None:
-        print("\n[INFO] No Person Detected In This Image, EXIT!")
-        exit()
+#     container3 = xmlrpc.client.ServerProxy('http://0.0.0.0:11000')
+#     human_segmentation = container3.Predict(image_str)
+#     print("\n[INFO] Human Extraction FINISHED")
+#     if human_segmentation==None:
+#         print("\n[INFO] No Person Detected In This Image, EXIT!")
+#         exit()
 
-    container4 = xmlrpc.client.ServerProxy('http://0.0.0.0:12000')
-    sleeping = container4.Predict(human_segmentation)
-    print("\n[INFO] Pose Analysis FINISHED")
-    if sleeping:
-        print("\n[PREDICTION] The Driver Is Likely To Be Sleeping!")
-    else:
-        print("\n[PREDICTION] The Driver Is NOT Likely To Be Sleepling.")
+#     container4 = xmlrpc.client.ServerProxy('http://0.0.0.0:12000')
+#     sleeping = container4.Predict(human_segmentation)
+#     print("\n[INFO] Pose Analysis FINISHED")
+#     if sleeping:
+#         print("\n[PREDICTION] The Driver Is Likely To Be Sleeping!")
+#     else:
+#         print("\n[PREDICTION] The Driver Is NOT Likely To Be Sleepling.")
 
 
 if __name__ == "__main__":
