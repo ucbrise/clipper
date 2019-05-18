26f967ee377cb3856f867dc2e874e4ba692afaab
Fred <yy0125@connect.hku.hk>
2019-05-18 15:45:29 +0800

    a

--- applications/prediction/main.py
+++ applications/prediction/main.py
@@ -1,6 +1,22 @@
 import pandas as pd
 import numpy as np
 
+import sys
+sys.path.append("/container")
+
+import c1_Stock_Price_Retriever.app.predict as c1
+import c2_Twitter_Collector.app.predict as c2 
+import c3_Tokenizer.app.predict as c3
+import c4_Sentiment_Analysis.app.predict as c4 
+import c5_LSTM_Predictor.app.predict as c5
+import c6_Mem.app.predict as c6 
+import c7_ARIMA.app.predict as c7 
+import c8_KNN.app.predict as c8 
+import c9_RandomForest.app.predict as c9 
+import c10_Regression.app.predict as c10 
+import c11_Conclusion.app.predict as c11 
+print("Modules successfully loaded!")
+
 def run():
     print("\nStart Prediciting: ")
 
@@ -8,74 +24,64 @@ def run():
     print("\nStock name: " + stock_name)
 
     # CONTAINER 1: stock price retriever
-    container1 = xmlrpc.client.ServerProxy('http://localhost:8000')
-    stock_data = pd.read_json(container1.Predict(stock_name))
+    stock_data = pd.read_json(c1.predict(stock_name))
     print("\nStock price data Retrieval FINISHED")
     print("The retrieved data is in shape of ", stock_data.shape)
     print("Here are the first 5 lines of retrieved data:")
     print(stock_data.head())
 
     # CONTAINER 5: LSTM
-    container5 = xmlrpc.client.ServerProxy('http://localhost:13000')
-    result_lstm = container5.Predict(stock_data.to_json())
+    result_lstm = c5.predict(stock_data.to_json())
     print("\nPrediction using LSTM FINISHED")
     print("Here is the result:")
     print(result_lstm)
 
     # CONTAINER 7: ARIMA
-    container7 = xmlrpc.client.ServerProxy('http://localhost:15000')
-    result_arima = container7.Predict(stock_data.to_json())
+    result_arima = c7.predict(stock_data.to_json())
     print("\nPrediction using ARIMA FINISHED")
     print("Here is the result:")
     print(result_arima)
 
     # CONTAINER 8: KNN
-    container8 = xmlrpc.client.ServerProxy('http://localhost:16000')
-    result_knn = container8.Predict(stock_data.to_json())
+    result_knn = c8.predict(stock_data.to_json())
     print("\nPrediction using KNN FINISHED")
     print("Here is the result:")
     print(result_knn)
 
     # CONTAINER 9: Random Forest
-    container9 = xmlrpc.client.ServerProxy('http://localhost:17000')
-    result_rf = container9.Predict(stock_data.to_json())
+    result_rf = c9.predict(stock_data.to_json())
     print("\nPrediction using Random Forest FINISHED")
     print("Here is the result:")
     print(result_rf)
 
     # CONTAINER 10: Regression
-    container10 = xmlrpc.client.ServerProxy('http://localhost:18000')
-    result_rg = container10.Predict(stock_data.to_json())
+    result_rg = c10.predict(stock_data.to_json())
     print("\nPrediction using Regrerssion FINISHED")
     print("Here is the result:")
     print(result_rg)
 
     # CONTAINER 2: Twitter Collector
-    container2 = xmlrpc.client.ServerProxy('http://localhost:9001')
     tweet_number = 1000
-    twitter_data = container2.Predict(stock_name, tweet_number)
+    twitter_data = c2.predict(stock_name, tweet_number)
     print("\nTwitter data Retrieval FINISHED")
     print("Successfully retrieved", tweet_number, "number of tweets.")
     print("Here are the first 200 characters:")
     print(twitter_data[:200])
 
     # CONTAINER 3: Tokenizer
-    container3 = xmlrpc.client.ServerProxy('http://localhost:11000')
-    tokenized_twitter_data = container3.Predict(twitter_data)
+    tokenized_twitter_data = c3.predict(twitter_data)
     print("\nTokenization FINISHED")
     print("Generated a list containing ", len(tokenized_twitter_data), " sentences")
     print("The first sentence is :\n", tokenized_twitter_data[0])
 
     # CONTAINER 4: sentimental Analysis
-    container4 = xmlrpc.client.ServerProxy('http://localhost:12000')
-    polarity_list = container4.Predict(tokenized_twitter_data)
+    polarity_list = c4.predict(tokenized_twitter_data)
     print("\nTwitter data Sentiment Analysis FINISHED")
     print("Generated a list containing ", len(polarity_list), " results")
     print("The first result is :\n", polarity_list[0])
 
     # CONTAINER 11: Weighting Algorithm
-    container11 = xmlrpc.client.ServerProxy('http://localhost:19000')
-    final_prediction = container11.Predict([result_arima, result_knn, result_lstm, result_rg], result_rf, polarity_list )
+    final_prediction = c11.predict([result_arima, result_knn, result_lstm, result_rg], result_rf, polarity_list )
     print("\n\nENTIRE PROCESS FINISHED")
     print("HERE IS THE FINAL PREDICTION FOR THE STOCK PRICES OF THE NEXT FEW DAYS:")
     print(final_prediction)
