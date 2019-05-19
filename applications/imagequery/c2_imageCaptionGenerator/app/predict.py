1a3a66dc13971d3d2b44a39b54648f150b0f8525
Fred <yy0125@connect.hku.hk>
2019-05-19 21:39:20 +0800

    a

--- applications/imagequery/c2_imageCaptionGenerator/app/predict.py
+++ applications/imagequery/c2_imageCaptionGenerator/app/predict.py
@@ -36,7 +36,7 @@ def predict(image_file_index):
     return "Invalid image file index! Only index between 1 to 800 is allowed!"
   
   start = timer()
-  image_file_path = "/container/c2_imageCaptionGenerator/im2txt/data/imageDataset/image_" + str(image_file_index).zfill(4) + ".jpg"
+  image_file_path = "/container/c2_imageCaptionGenerator/im2txt/data/imageDataset/101_ObjectCategories/image_" + str(image_file_index).zfill(4) + ".jpg"
   predictCmd = "python " + run_inference_path + " --checkpoint_path " + check_point_path + " --vocab_file " + vocabulary_path + " --input_files " + image_file_path
   os.system(predictCmd)
   generated_caption = getGeneratedCaptions()
