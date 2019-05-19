13c276913d07f5f6d7b53fa01915152e7c57e3d3
Fred <yy0125@connect.hku.hk>
2019-05-19 09:36:18 +0800

    get returned value from process ok

--- applications/imagequery/main.py
+++ applications/imagequery/main.py
@@ -9,14 +9,14 @@ import c3_nlpMappingGenerator.app.predict as mapping_generator
 import c4_questionAnswering.app.predict as question_answerer
 print("Modules successfully imported!")
 
-def run_speech_recognition(audio_file_path, result_list, elapsed_time_list):
+def run_speech_recognition(audio_file_path):
   speech_text, elapsed_time = speech_recognizer.predict(audio_file_path)
   print("1:\tText: " + speech_text)
   result_list.append(speech_text)
   elapsed_time_list.append(elapsed_time)
   return speech_text, elapsed_time
 
-def generate_image_caption(image_file, result_list, elapsed_time_list):
+def generate_image_caption(image_file):
   captions, elapsed_time = caption_generator.predict("image.jpg")
   print("2:\tGenerated captions: " + captions)
   result_list.append(captions)
@@ -27,21 +27,21 @@ def run():
   elapsed_time_list = []
   result_list = []
   p = Pool(2)
-  returned_result1 = p.apply_async(run_speech_recognition, args=("/container/c1_speechRecognition/app/speech.wav", result_list, elapsed_time_list))
-  returned_result2 = p.apply_async(generate_image_caption, args=("image.jpg", result_list, elapsed_time_list))
+  returned_result1 = p.apply_async(run_speech_recognition, args=("/container/c1_speechRecognition/app/speech.wav",))
+  returned_result2 = p.apply_async(generate_image_caption, args=("image.jpg",))
   p.close()
   p.join() # p.join()方法会等待所有子进程执行完毕
 
   result1 = returned_result1.get()[0]
   time1 = returned_result1.get()[1]
-  print(result_list)
-  print(elapsed_time_list)
-  print(returned_result1)
-  print(result1)
-  print(time1)
+  result2 = returned_result2.get()[0]
+  time2 = returned_result2.get()[1]
+  elapsed_time_list.append(time1)
+  elapsed_time_list.append(time2)
 
   # CONTAINER 3: image nlp analyzer
-  text = result_list[0] + "." + result_list[1]
+  text = result1 + "." + result2
+  print("Input to mapping generator: " + text)
   mapping, elapsed_time = mapping_generator.predict(text)
   elapsed_time_list.append(elapsed_time)
   print("3:\tGenerated mapping: ")
