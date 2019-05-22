6fb3c8bfa2eaf44269f4f2aad6a4b80a826ac538
Fred <yy0125@connect.hku.hk>
2019-05-22 20:07:01 +0800

    make c1 takes 1-800 index

--- applications/imagequery/c1_speechRecognition/app/predict.py
+++ applications/imagequery/c1_speechRecognition/app/predict.py
@@ -4,10 +4,15 @@ from timeit import default_timer as timer
 # Text2Speech converter: https://www.text2speech.org/
 
 def recognize(audio_file_index):
-    if audio_file_index < 1 or audio_file_index > 500:
-        return "Invalid image index! Only index between 1 to 500 is allowed! Exiting..."
-    
-    audio_file_path = "/container/c1_speechRecognition/data/cmu_us_awb_arctic/wav/arctic_a" + str(audio_file_index).zfill(4) + ".wav"
+    if audio_file_index < 1 or audio_file_index > 800:
+        return "Invalid image index! Only index between 1 to 800 is allowed! Exiting..."
+
+    if audio_file_index > 500: 
+        audio_file_path = "/container/c1_speechRecognition/data/cmu_us_awb_arctic/wav/arctic_a" + str(audio_file_index).zfill(4) + ".wav"
+    else:
+        audio_file_path = "/container/c1_speechRecognition/data/cmu_us_awb_arctic/wav/arctic_b + str(audio_file_index - 500).zfill(4) + ".wav"
+
+    print(audio_file_path)
     recognizer = sr.Recognizer()
     audio_file = sr.AudioFile(audio_file_path)
 
