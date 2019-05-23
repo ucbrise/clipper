43efa8014d78c476818db8a1021e77da48672a11
Fred <yy0125@connect.hku.hk>
2019-05-23 15:11:18 +0800

    modify build script

--- applications/build_docker_imagequery.sh
+++ applications/build_docker_imagequery.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
+docker build -f ./imagequery/Dockerfile0 -t imagequery:container0 .
 docker build -f ./imagequery/Dockerfile1 -t imagequery:container1 .
 docker build -f ./imagequery/Dockerfile2 -t imagequery:container2 .
 docker build -f ./imagequery/Dockerfile3 -t imagequery:container3 .
