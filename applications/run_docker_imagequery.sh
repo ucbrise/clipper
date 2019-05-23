be6dcce23756f5b193be1741ab71403c31a75c9a
Fred <yy0125@connect.hku.hk>
2019-05-23 16:13:47 +0800

    update Dockerfiles

--- applications/run_docker_imagequery.sh
+++ applications/run_docker_imagequery.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #/bin/sh
 
+docker run -p 7000:7000 -it imagequery:container0 & 
 docker run -p 8000:8000 -it imagequery:container1  &
 docker run --runtime=nvidia -p 9000:9000 -it imagequery:container2  &
 docker run -p 11000:11000 -it imagequery:container3  &
@@ -8,6 +9,7 @@ docker run -p 12000:12000 -it imagequery:container4  &
 
 
 # in detached mode
+docker run -d -p 7000:7000 -it imagequery:container0 & 
 docker run -d -p 8000:8000 imagequery:container1  &
 docker run --runtime=nvidia -d -p 9000:9000 imagequery:container2  &
 docker run -d -p 11000:11000 imagequery:container3  &
