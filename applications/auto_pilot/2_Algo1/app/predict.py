870db01ec50b5fe30fa6e89f96ea4fb6b986d386
Tianhao Zhang <dumpkin1996@gmail.com>
2019-05-24 22:56:14 +0800

    minor changes

--- applications/auto_pilot/2_Algo1/app/predict.py
+++ applications/auto_pilot/2_Algo1/app/predict.py
@@ -8,9 +8,9 @@ saver = tf.train.Saver()
 saver.restore(sess, "/container/model.ckpt")
 
 def predict(image_str):
-	fh = open("temp.jpg", "wb")
-	fh.write(base64.decodebytes(image_str))
-	fh.close()
+    fh = open("temp.jpg", "wb")
+    fh.write(base64.decodebytes(image_str))
+    fh.close()
     image = scipy.misc.imread("temp.jpg", mode="RGB").tolist()
     image = scipy.misc.imresize(image[-150:], [66, 200]) / 255.0
     degrees = model.y.eval(feed_dict={model.x: [image], model.keep_prob: 1.0})[0][0] * 180.0 / 3.1415926
