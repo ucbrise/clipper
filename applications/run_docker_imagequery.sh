e08d8fac011c3c74132b1cb2599978c5da43e2d1
Fred <yy0125@connect.hku.hk>
2019-05-24 17:04:33 +0800

    update

--- applications/run_docker_imagequery.sh
+++ applications/run_docker_imagequery.sh
@@ -3,7 +3,7 @@
 
 docker run -p 7000:7000 -it imagequery:container0 & 
 docker run -p 8000:8000 -it imagequery:container1  &
-docker run --runtime=nvidia -p 9000:9000 -it imagequery:container2  &
+docker run --runtime=nvidia -e MODEL_NAME=test -e MODEL_PORT=22222 -p 9000:9000 -it imagequery:container2 &
 docker run -p 11000:11000 -it imagequery:container3  &
 docker run -p 12000:12000 -it imagequery:container4  &
 
