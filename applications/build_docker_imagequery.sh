a13f10c29b13cf86ff81e81d93f17d81a02ed056
Fred <yy0125@connect.hku.hk>
2019-05-24 14:03:42 +0800

    allow c1 use cache

--- applications/build_docker_imagequery.sh
+++ applications/build_docker_imagequery.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 docker build -f ./imagequery/Dockerfile0 -t imagequery:container0 .
-docker build --no-cache -f ./imagequery/Dockerfile1 -t imagequery:container1 .
+docker build -f ./imagequery/Dockerfile1 -t imagequery:container1 .
 docker build -f ./imagequery/Dockerfile2 -t imagequery:container2 .
 docker build -f ./imagequery/Dockerfile3 -t imagequery:container3 .
 docker build -f ./imagequery/Dockerfile4 -t imagequery:container4 .
