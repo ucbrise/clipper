f9f809f4a1589faff7a279755e5fb943b2c74a60
Tianhao Zhang <dumpkin1996@gmail.com>
2019-05-26 00:07:40 +0800

    minor changes

--- applications/auto_pilot/2_Algo1/app/predict.py
+++ applications/auto_pilot/2_Algo1/app/predict.py
@@ -8,15 +8,16 @@ saver.restore(sess, "/container/model.ckpt")
 
 def read_image(i):
 	image_path = "/container/dataset/" + i + ".jpg"
-	print("TESTING WHETHER PRINT WORKS")
 	print(image_path)
 	image = scipy.misc.imread(image_path, mode="RGB").tolist()
+	print("image shape is ", image.shape)
 	return image
 
 def predict(i):
 	try:
 		image = read_image(i)
 		image = scipy.misc.imresize(image[-150:], [66, 200]) / 255.0
+		print("resized image shape is ", image.shape)
 		degrees = model.y.eval(feed_dict={model.x: [image], model.keep_prob: 1.0})[0][0] * 180.0 / 3.1415926
 		print(degrees)
 		return str(degrees)
