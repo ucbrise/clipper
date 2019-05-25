2461049df229a68228aec287090880f16d9d3f87
Sungjun, Kim <sungjunkim@linecorp.com>
2019-05-26 00:29:01 +0900

    Cleanup all containers and images when starting CI

--- bin/cleanup_jenkins.sh
+++ bin/cleanup_jenkins.sh
@@ -5,9 +5,9 @@ CLIPPER_REGISTRY=clippertesting
 
 # best effort, ignore all errors
 try_cleanup() {
-    (docker ps | grep "$@" | awk '{ print $1 }' | xargs docker kill) || true
-    (docker container ls | grep "$@" | awk '{ print $3 }' | xargs docker container rm) || true
-    (docker image ls | grep "$@" | awk '{ print $3 }' | xargs docker image rm -f) || true
+    (docker ps --all | grep "$@" | awk '{ print $1 }' | xargs docker kill) || true
+    (docker container ls --all | grep "$@" | awk '{ print $3 }' | xargs docker container rm) || true
+    (docker image ls --all | grep "$@" | awk '{ print $3 }' | xargs docker image rm -f) || true
 }
 
 try_cleanup CLIPPER_REGISTRY > /dev/null
