3b7e3cabb65d73b355946a0555827aeff21cb87a
Fred <yy0125@connect.hku.hk>
2019-05-25 17:29:00 +0800

    new predict.py for imagequery c3

--- applications/imagequery/c3_nlpMappingGenerator/app/predict.py
+++ applications/imagequery/c3_nlpMappingGenerator/app/predict.py
@@ -1,94 +1,314 @@
-# Ref1: spacy official documenation for tagging, noun_chunk 
-# Ref2: textacy official documentation for matching pattern
-import spacy
-import textacy
-# from c3_nlpMappingGenerator.app.preprocess import preprocess
-from preprocess import preprocess
-from timeit import default_timer as timer
+import en_core_web_md
 
-# def print_document(doc):
-#   import pprint
-#   pp = pprint.PrettyPrinter()
-#   print('\n')
-#   for token in doc:
-#       pp.pprint((token.text, token.lemma_, token.pos_,
-#                   token.tag_, token.dep_, token.is_stop))
+# use spacy small model
+nlp = en_core_web_md.load()
+
+# dependency markers for subjects
+SUBJECTS = {"nsubj", "nsubjpass", "csubj", "csubjpass", "agent", "expl"}
+# dependency markers for objects
+OBJECTS = {"dobj", "dative", "attr", "oprd"}
+# POS tags that will break adjoining items
+BREAKER_POS = {"CCONJ", "VERB"}
+# words that are negations
+NEGATIONS = {"no", "not", "n't", "never", "none"}
 
 
-# def print_verb_tagging(doc):
-#     print("\nTokens with 'VERB' as POS tag: ")
-#     for token in doc:
-#         if (token.pos_ == 'VERB'):
-#             print((token.text, token.pos_))
+# does dependency set contain any coordinating conjunctions?
+def contains_conj(depSet):
+    return "and" in depSet or "or" in depSet or "nor" in depSet or \
+           "but" in depSet or "yet" in depSet or "so" in depSet or "for" in depSet
 
 
-def get_time_list(doc):
-    time_list = []
-    for time in doc.ents:
-        if time.label_ == 'TIME' or time.label_ == 'DATE':
-            time_list.append(time.text)
-    return time_list
+# get subs joined by conjunctions
+def _get_subs_from_conjunctions(subs):
+    more_subs = []
+    for sub in subs:
+        # rights is a generator
+        rights = list(sub.rights)
+        rightDeps = {tok.lower_ for tok in rights}
+        if contains_conj(rightDeps):
+            more_subs.extend([tok for tok in rights if tok.dep_ in SUBJECTS or tok.pos_ == "NOUN"])
+            if len(more_subs) > 0:
+                more_subs.extend(_get_subs_from_conjunctions(more_subs))
+    return more_subs
 
 
-def get_noun_chunk_list(doc):
-    noun_chunk_list = []
-    for chunk in doc.noun_chunks:
-        noun_chunk_list.append(chunk.text)
-    return noun_chunk_list
+# get objects joined by conjunctions
+def _get_objs_from_conjunctions(objs):
+    more_objs = []
+    for obj in objs:
+        # rights is a generator
+        rights = list(obj.rights)
+        rightDeps = {tok.lower_ for tok in rights}
+        if contains_conj(rightDeps):
+            more_objs.extend([tok for tok in rights if tok.dep_ in OBJECTS or tok.pos_ == "NOUN"])
+            if len(more_objs) > 0:
+                more_objs.extend(_get_objs_from_conjunctions(more_objs))
+    return more_objs
 
 
-def get_verb_phrase_list(txt):
-    """
-    ? : 0 or 1
-    + : >= 1
-    * : >= 0
-    """
-    # Though you can create your own patter, they easily overshoot
-    pattern = r'<VERB>*<ADV>*<PART>*<VERB>+<PART>*'
-    doc = textacy.make_spacy_doc(txt, lang='en_core_web_md')
-    lists = textacy.extract.pos_regex_matches(doc, pattern)
-    verb_phrases = []
-    for list in lists:
-        verb_phrases.append(list.text)
-    return verb_phrases
+# find sub dependencies
+def _find_subs(tok):
+    head = tok.head
+    while head.pos_ != "VERB" and head.pos_ != "NOUN" and head.head != head:
+        head = head.head
+    if head.pos_ == "VERB":
+        subs = [tok for tok in head.lefts if tok.dep_ == "SUB"]
+        if len(subs) > 0:
+            verb_negated = _is_negated(head)
+            subs.extend(_get_subs_from_conjunctions(subs))
+            return subs, verb_negated
+        elif head.head != head:
+            return _find_subs(head)
+    elif head.pos_ == "NOUN":
+        return [head], _is_negated(tok)
+    return [], False
 
 
-def generate_mapping(txt):
-    nlp = spacy.load('en_core_web_md')
-    txt = preprocess(txt)
-    document = nlp(txt)
-
-    noun_chunk_list = get_noun_chunk_list(document)
-    verb_phrase_list = get_verb_phrase_list(txt)
-    time_list = get_time_list(document)
-
-    subject = noun_chunk_list[0] if len(noun_chunk_list) > 0 else "<No subject>"
-    verb_phrase = verb_phrase_list[0] if len(verb_phrase_list) > 0 else "<No predicate>"
-    time = time_list[0] if len(time_list) > 0 else "<No time>"
-    mapping_string = subject + "-" + verb_phrase + "-" + time 
-    """ Format: <subject>-<verb>-<time> """
-    return mapping_string
+# is the tok set's left or right negated?
+def _is_negated(tok):
+    parts = list(tok.lefts) + list(tok.rights)
+    for dep in parts:
+        if dep.lower_ in NEGATIONS:
+            return True
+    return False
 
 
-def testing():
-    txt1 = "A man is happily playing basketball at 3 o'clock."
-    txt2 = "A boy is crying silently at night."
-    txt3 = "An athelete is running very fast. It was April 1996"
-    txt4 = "A woman is running quickly in the morning."
+# get all the verbs on tokens with negation marker
+def _find_svs(tokens):
+    svs = []
+    verbs = [tok for tok in tokens if tok.pos_ == "VERB"]
+    for v in verbs:
+        subs, verbNegated = _get_all_subs(v)
+        if len(subs) > 0:
+            for sub in subs:
+                svs.append((sub.orth_, "!" + v.orth_ if verbNegated else v.orth_))
+    return svs
 
-    txt_list = [txt1, txt2, txt3, txt4]
-    for index, txt in enumerate(txt_list, start=1):
-        """ the index here should is necessary!!! """
-        print(generate_mapping(txt))
-        print(generate_mapping(txt).split('-'))
 
-def predict(txt):
-    start = timer()
-    generated_mapping = generate_mapping(txt)
-    end = timer()
-    time_elapsed = end - start
-    return generated_mapping
+# get grammatical objects for a given set of dependencies (including passive sentences)
+def _get_objs_from_prepositions(deps, is_pas):
+    objs = []
+    for dep in deps:
+        if dep.pos_ == "ADP" and (dep.dep_ == "prep" or (is_pas and dep.dep_ == "agent")):
+            objs.extend([tok for tok in dep.rights if tok.dep_  in OBJECTS or
+                         (tok.pos_ == "PRON" and tok.lower_ == "me") or
+                         (is_pas and tok.dep_ == 'pobj')])
+    return objs
 
-# if __name__ == "__main__":
-#     txt = "a man riding a wave on top of a surfboard . a person riding a surf board on a wave a man on a surfboard riding a wave ."
-#     print(predict(txt)) # man-happily play-3 o'clock
+
+# get objects from the dependencies using the attribute dependency
+def _get_objs_from_attrs(deps, is_pas):
+    for dep in deps:
+        if dep.pos_ == "NOUN" and dep.dep_ == "attr":
+            verbs = [tok for tok in dep.rights if tok.pos_ == "VERB"]
+            if len(verbs) > 0:
+                for v in verbs:
+                    rights = list(v.rights)
+                    objs = [tok for tok in rights if tok.dep_ in OBJECTS]
+                    objs.extend(_get_objs_from_prepositions(rights, is_pas))
+                    if len(objs) > 0:
+                        return v, objs
+    return None, None
+
+
+# xcomp; open complement - verb has no suject
+def _get_obj_from_xcomp(deps, is_pas):
+    for dep in deps:
+        if dep.pos_ == "VERB" and dep.dep_ == "xcomp":
+            v = dep
+            rights = list(v.rights)
+            objs = [tok for tok in rights if tok.dep_ in OBJECTS]
+            objs.extend(_get_objs_from_prepositions(rights, is_pas))
+            if len(objs) > 0:
+                return v, objs
+    return None, None
+
+
+# get all functional subjects adjacent to the verb passed in
+def _get_all_subs(v):
+    verb_negated = _is_negated(v)
+    subs = [tok for tok in v.lefts if tok.dep_ in SUBJECTS and tok.pos_ != "DET"]
+    if len(subs) > 0:
+        subs.extend(_get_subs_from_conjunctions(subs))
+    else:
+        foundSubs, verb_negated = _find_subs(v)
+        subs.extend(foundSubs)
+    return subs, verb_negated
+
+
+# is the token a verb?  (excluding auxiliary verbs)
+def _is_non_aux_verb(tok):
+    return tok.pos_ == "VERB" and (tok.dep_ != "aux" and tok.dep_ != "auxpass")
+
+
+# return the verb to the right of this verb in a CCONJ relationship if applicable
+# returns a tuple, first part True|False and second part the modified verb if True
+def _right_of_verb_is_conj_verb(v):
+    # rights is a generator
+    rights = list(v.rights)
+
+    # VERB CCONJ VERB (e.g. he beat and hurt me)
+    if len(rights) > 1 and rights[0].pos_ == 'CCONJ':
+        for tok in rights[1:]:
+            if _is_non_aux_verb(tok):
+                return True, tok
+
+    return False, v
+
+
+# get all objects for an active/passive sentence
+def _get_all_objs(v, is_pas):
+    # rights is a generator
+    rights = list(v.rights)
+
+    objs = [tok for tok in rights if tok.dep_ in OBJECTS or (is_pas and tok.dep_ == 'pobj')]
+    objs.extend(_get_objs_from_prepositions(rights, is_pas))
+
+    #potentialNewVerb, potentialNewObjs = _get_objs_from_attrs(rights)
+    #if potentialNewVerb is not None and potentialNewObjs is not None and len(potentialNewObjs) > 0:
+    #    objs.extend(potentialNewObjs)
+    #    v = potentialNewVerb
+
+    potential_new_verb, potential_new_objs = _get_obj_from_xcomp(rights, is_pas)
+    if potential_new_verb is not None and potential_new_objs is not None and len(potential_new_objs) > 0:
+        objs.extend(potential_new_objs)
+        v = potential_new_verb
+    if len(objs) > 0:
+        objs.extend(_get_objs_from_conjunctions(objs))
+    return v, objs
+
+
+# return true if the sentence is passive - at he moment a sentence is assumed passive if it has an auxpass verb
+def _is_passive(tokens):
+    for tok in tokens:
+        if tok.dep_ == "auxpass":
+            return True
+    return False
+
+
+# resolve a 'that' where/if appropriate
+def _get_that_resolution(toks):
+    for tok in toks:
+        if 'that' in [t.orth_ for t in tok.lefts]:
+            return tok.head
+    return toks
+
+
+# simple stemmer using lemmas
+def _get_lemma(word: str):
+    tokens = nlp(word)
+    if len(tokens) == 1:
+        return tokens[0].lemma_
+    return word
+
+
+# print information for displaying all kinds of things of the parse tree
+def printDeps(toks):
+    for tok in toks:
+        print(tok.orth_, tok.dep_, tok.pos_, tok.head.orth_, [t.orth_ for t in tok.lefts], [t.orth_ for t in tok.rights])
+
+
+# expand an obj / subj np using its chunk
+def expand(item, tokens):
+    if item.lower_ == 'that':
+        item = _get_that_resolution(tokens)
+
+    parts = []
+
+    if hasattr(item, 'lefts'):
+        for part in item.lefts:
+            if part.pos_ in BREAKER_POS:
+                break
+            if not part.lower_ in NEGATIONS:
+                parts.append(part)
+
+    parts.append(item)
+
+    if hasattr(item, 'rights'):
+        for part in item.rights:
+            if part.pos_ in BREAKER_POS:
+                break
+            if not part.lower_ in NEGATIONS:
+                parts.append(part)
+
+    if hasattr(parts[-1], 'rights'):
+        for item2 in parts[-1].rights:
+            if item2.pos_ == "DET" or item2.pos_ == "NOUN":
+                parts.extend(expand(item2, tokens))
+            break
+
+    return parts
+
+
+# convert a list of tokens to a string
+def to_str(tokens):
+    return ' '.join([item.text for item in tokens])
+
+
+# find verbs and their subjects / objects to create SVOs, detect passive/active sentences
+def findSVOs(tokens):
+    svos = []
+    is_pas = _is_passive(tokens)
+    verbs = [tok for tok in tokens if _is_non_aux_verb(tok)]
+    for v in verbs:
+        subs, verbNegated = _get_all_subs(v)
+        # hopefully there are subs, if not, don't examine this verb any longer
+        if len(subs) > 0:
+            isConjVerb, conjV = _right_of_verb_is_conj_verb(v)
+            if isConjVerb:
+                v2, objs = _get_all_objs(conjV, is_pas)
+                for sub in subs:
+                    for obj in objs:
+                        objNegated = _is_negated(obj)
+                        if is_pas:  # reverse object / subject for passive
+                            svos.append((to_str(expand(obj, tokens)),
+                                         "!" + v.lemma_ if verbNegated or objNegated else v.lemma_, to_str(expand(sub, tokens))))
+                            svos.append((to_str(expand(obj, tokens)),
+                                         "!" + v2.lemma_ if verbNegated or objNegated else v2.lemma_, to_str(expand(sub, tokens))))
+                        else:
+                            svos.append((to_str(expand(sub, tokens)),
+                                         "!" + v.lower_ if verbNegated or objNegated else v.lower_, to_str(expand(obj, tokens))))
+                            svos.append((to_str(expand(sub, tokens)),
+                                         "!" + v2.lower_ if verbNegated or objNegated else v2.lower_, to_str(expand(obj, tokens))))
+            else:
+                v, objs = _get_all_objs(v, is_pas)
+                for sub in subs:
+                    for obj in objs:
+                        objNegated = _is_negated(obj)
+                        if is_pas:  # reverse object / subject for passive
+                            svos.append((to_str(expand(obj, tokens)),
+                                         "!" + v.lemma_ if verbNegated or objNegated else v.lemma_, to_str(expand(sub, tokens))))
+                        else:
+                            svos.append((to_str(expand(sub, tokens)),
+                                         "!" + v.lower_ if verbNegated or objNegated else v.lower_, to_str(expand(obj, tokens))))
+    return svos
+
+def predict(input_str):
+    tokens = nlp(input_str)
+    svos = findSVOs(tokens)
+    combined_subject = "<"
+    combined_verb = "<"
+    combined_object = "<"
+    i = 0
+    for svo in svos:
+        if i < len(svos) - 1:
+            combined_subject = combined_subject + svo[0] + ", "
+            combined_verb = combined_verb  + svo[1] + ", "
+            combined_object = combined_object  + svo[2] + ", "
+        else:
+            combined_subject = combined_subject + svo[0]
+            combined_verb = combined_verb  + svo[1]
+            combined_object = combined_object  + svo[2]
+        i = i + 1
+    combined_subject += ">"
+    combined_verb += ">"
+    combined_object += ">"
+    print(combined_object)
+    print(combined_subject)
+    print(combined_verb)
+    
+    return combined_subject + combined_verb + combined_object
+
+if __name__ == "__main__":
+    print(predict)
