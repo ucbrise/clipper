2abc8d02f05d0d1f67fcae71b2e27332aa8c59f1
Tianhao Zhang <dumpkin1996@gmail.com>
2019-05-25 23:46:57 +0800

    minor changes

--- applications/auto_pilot/2_Algo1/app/predict.py
+++ applications/auto_pilot/2_Algo1/app/predict.py
@@ -8,6 +8,8 @@ saver.restore(sess, "/container/model.ckpt")
 
 def read_image(i):
 	image_path = "/container/dataset/" + i + ".jpg"
+	print("TESTING WHETHER PRINT WORKS")
+	print(image_path)
 	image = scipy.misc.imread(image_path, mode="RGB").tolist()
 	return image
 
@@ -15,4 +17,5 @@ def predict(i):
 	image = read_image(i)
 	image = scipy.misc.imresize(image[-150:], [66, 200]) / 255.0
 	degrees = model.y.eval(feed_dict={model.x: [image], model.keep_prob: 1.0})[0][0] * 180.0 / 3.1415926
+	print(degrees)
 	return str(degrees)
