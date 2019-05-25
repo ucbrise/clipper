f8702494a4d2c570dd1aabee5d5e0988c7c790d0
Tianhao Zhang <dumpkin1996@gmail.com>
2019-05-25 23:24:46 +0800

    cut a a container

--- applications/auto_pilot/6_Conclusion/app/predict.py
+++ applications/auto_pilot/6_Conclusion/app/predict.py
@@ -1,25 +1,15 @@
 
-def predict(a1_str, a2_str, a3_str, a4_str):
+def predict(a1_str, a2_str, a3_str):
 
     a1 = float(a1_str)
     a2 = float(a2_str)
     a3 = float(a3_str)
-    a4 = float(a4_str)
 
-    a_list = [a1, a2, a3, a4]
-
-    average = sum(a_list) / 4
-
-    deviations = [ abs(x-average) for x in a_list ]
-
-    a_list.remove(a_list[deviations.index(max(deviations))])
-
-    final = sum(a_list)/ len(a_list)
+    final = (a1 + a2 + a3) / 3
 
     result = "algo1 says " + str(round(a1,2))
     result += ", algo2 says " + str(round(a2,2))
     result += ", algo3 says " + str(round(a3,2))
-    result += ", algo4 says " + str(round(a4,2))
     result += ", final is " + str(round(final,2))
 
     return result
