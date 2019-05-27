fb058a7afaec115780a04d612b5ffac0a37923fe
Sungjun, Kim <sungjunkim@linecorp.com>
2019-05-27 09:22:11 +0900

    Clean up old Docker images

--- bin/cleanup_jenkins.sh
+++ bin/cleanup_jenkins.sh
@@ -7,12 +7,13 @@ CLIPPER_REGISTRY=clippertesting
 try_cleanup() {
     (docker ps --all | grep "$@" | awk '{ print $1 }' | xargs docker kill) || true
     (docker container ls --all | grep "$@" | awk '{ print $3 }' | xargs docker container rm) || true
-    (docker image ls --all | grep "$@" | awk '{ print $3 }' | xargs docker image rm -f) || true
+    (docker image ls --filter "label=maintainer=Dan Crankshaw <dscrankshaw@gmail.com>" | awk '{ print $3 }' | xargs docker image rm -f) || true
+    docker image prune -f
 }
 
 try_cleanup_docker_volume() {
     (docker volume ls -f dangling=true | awk '{ print $2 }' | xargs docker volume rm -f) || true
 }
-try_cleanup CLIPPER_REGISTRY > /dev/null
+try_cleanup ${CLIPPER_REGISTRY} > /dev/null
 try_cleanup shipyard > /dev/null
 try_cleanup_docker_volume > /dev/null
