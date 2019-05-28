6f43ec96a25d6c62dd9e324eb3ebe35f21df4735
Fred <yy0125@connect.hku.hk>
2019-05-28 09:05:08 +0800

    update dir

---
+++ applications/imagequery/c1_speechRecognition/data/dataset3/rename.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /container/data/dataset3/recordings
+
+index=0;
+for name in *.mp3
+do
+    ffmpeg -i "${name}" "${index}.wav"
+    index=$((index+1))
+    if [ $index -gt 1000 ]; then
+      break
+    fi
+done
+
+echo "$(ls *.wav | wc -l) wav files in this dataset."
+# 1001 wav files
