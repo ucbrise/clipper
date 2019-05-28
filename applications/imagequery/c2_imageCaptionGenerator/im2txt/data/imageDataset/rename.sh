98d5e84d5a34620ed0c8d5ff8701e3fead7366b0
Fred <yy0125@connect.hku.hk>
2019-05-28 21:55:14 +0800

    update

--- applications/imagequery/c2_imageCaptionGenerator/im2txt/data/imageDataset/rename.sh
+++ applications/imagequery/c2_imageCaptionGenerator/im2txt/data/imageDataset/rename.sh
@@ -4,20 +4,82 @@ cd /container/im2txt/data/imageDataset/101_ObjectCategories
 
 # copy images from subdirectories to current directory: imageDatasets
 index=0;
+subcount=0
 for imageFile in ./airplanes/*.jpg
 do
     mv "${imageFile}" "${index}.jpg"
     index=$((index+1))
-    if [ $index -gt 1000 ]; then
+		subcount=$((subcount+1))
+    if [ $subcount -gt 100 ]; then
       break
     fi
 done
+subcount=0
 
+for imageFile in ./car_side/*.jpg
+do
+    mv "${imageFile}" "${index}.jpg"
+    index=$((index+1))
+		subcount=$((subcount+1))
+    if [ $subcount -gt 100 ]; then
+      break
+    fi
+done
+subcount=0
 
-# directories="directoryList.txt"
-# ls -d */ > $directories
-# while read directory; do
-# 	cp $directory/* .
-# done < $directories
+for imageFile in ./ferry/*.jpg
+do
+    mv "${imageFile}" "${index}.jpg"
+    index=$((index+1))
+		subcount=$((subcount+1))
+    if [ $subcount -gt 100 ]; then
+      break
+    fi
+done
+subcount=0
+
+for imageFile in ./Motobikes/*.jpg
+do
+    mv "${imageFile}" "${index}.jpg"
+    index=$((index+1))
+		subcount=$((subcount+1))
+    if [ $subcount -gt 100 ]; then
+      break
+    fi
+done
+subcount=0
+
+for imageFile in ./cougar_body/*.jpg
+do
+    mv "${imageFile}" "${index}.jpg"
+    index=$((index+1))
+		subcount=$((subcount+1))
+    if [ $subcount -gt 100 ]; then
+      break
+    fi
+done
+subcount=0
+
+for imageFile in ./camera/*.jpg
+do
+    mv "${imageFile}" "${index}.jpg"
+    index=$((index+1))
+		subcount=$((subcount+1))
+    if [ $subcount -gt 100 ]; then
+      break
+    fi
+done
+subcount=0
+
+for imageFile in ./butterfly/*.jpg
+do
+    mv "${imageFile}" "${index}.jpg"
+    index=$((index+1))
+		subcount=$((subcount+1))
+    if [ $subcount -gt 100 ]; then
+      break
+    fi
+done
+subcount=0
 
 echo "$(ls *.jpg | wc -l) jpg image files in this dataset."
