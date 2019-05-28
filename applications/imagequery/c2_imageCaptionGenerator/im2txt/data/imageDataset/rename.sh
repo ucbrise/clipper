4f0b7bacd53a58be3d30aebafa8554fab56e17ab
Fred <yy0125@connect.hku.hk>
2019-05-28 21:45:13 +0800

    update

--- applications/imagequery/c2_imageCaptionGenerator/im2txt/data/imageDataset/rename.sh
+++ applications/imagequery/c2_imageCaptionGenerator/im2txt/data/imageDataset/rename.sh
@@ -2,26 +2,22 @@
 
 cd /container/im2txt/data/imageDataset/101_ObjectCategories
 
-# copy all images from subdirectories to current directory: imageDatasets
-directories="directoryList.txt"
-ls -d */ > $directories
-while read directory; do
-	cp $directory/* .
-done < $directories
+# copy images from subdirectories to current directory: imageDatasets
+index=0;
+for imageFile in ./airplanes/*.jpg
+do
+    mv "./airplanes/${imageFile}" "${index}.wav"
+    index=$((index+1))
+    if [ $index -gt 1000 ]; then
+      break
+    fi
+done
 
-ls *.jpg 
+
+# directories="directoryList.txt"
+# ls -d */ > $directories
+# while read directory; do
+# 	cp $directory/* .
+# done < $directories
+
 echo "$(ls *.jpg | wc -l) jpg image files in this dataset."
-# No need to take care of index. Reindexed is performed automatically and neatly.
-
-# Reindex all jpg files in the current directory
-# imageFiles="jpgFiles.txt"
-# ls *.jpg > $imageFiles
-# let i=1
-# while read imageFile; do
-# 	mv $imageFile "image$i.jpg"
-# 	let i=$i+1
-# done < $imageFiles
-
-# remove temp files
-# rm $directories
-# rm $imageFiles
