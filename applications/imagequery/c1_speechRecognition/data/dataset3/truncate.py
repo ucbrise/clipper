cb18696487b969fa7b230a0ad9f536d46768955d
Fred <yy0125@connect.hku.hk>
2019-05-28 15:48:03 +0800

    truncate typo

--- applications/imagequery/c1_speechRecognition/data/dataset3/truncate.py
+++ applications/imagequery/c1_speechRecognition/data/dataset3/truncate.py
@@ -6,19 +6,20 @@ import glob
 wav_files = []
 for file in glob.glob("./*.wav"):
     wav_files.append(file)
-print(wav_files)
 
-print("audio_files successfully generated!")
+# print(wav_files)
+
+# print("audio_files successfully generated!")
 
 from pydub import AudioSegment
 
-print("pydub successfully imported!")
+# print("pydub successfully imported!")
 
 for wav_file in wav_files:
     original = AudioSegment.from_wav(wav_file)
     start_time = 0
     end_time = 5 * 1000
     truncated = original[start_time:end_time]
-    word.export(wav_file, format="wav")
+    truncated.export(wav_file, format="wav")
   
 print("audio files successfully truncated!")
