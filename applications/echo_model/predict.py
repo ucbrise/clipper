58557a5fa66c36ea071dd8f1a98fc77904d8096d
Tianhao Zhang <dumpkin1996@gmail.com>
2019-06-02 22:41:45 +0800

    changes

--- applications/echo_model/predict.py
+++ applications/echo_model/predict.py
@@ -3,8 +3,8 @@ from time import sleep
 def predict(input):
 	sleep_time = int(input.split("***")[0])
 	prevent_cache = input.split("***")[1]
-    sleep(sleep_time/1000)
-    return input
+	sleep(sleep_time/1000)
+	return input
 
 
 
