316af5684e7675b3c38b97e05b9f00be36efbd31
Fred <yy0125@connect.hku.hk>
2019-06-02 18:56:55 +0800

    change file location

--- applications/imagequery_echo_model/build_docker_imagequery_echo.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-docker build -f ./Dockerfile0 -t imagequery_echo_model:container0 .
-docker build -f ./Dockerfile1 -t imagequery_echo_model:container1 .
-docker build -f ./Dockerfile2 -t imagequery_echo_model:container2 .
-docker build -f ./Dockerfile3 -t imagequery_echo_model:container3 .
-docker build -f ./Dockerfile4 -t imagequery_echo_model:container4 .
