23a6e63552aaff4569f641e69c69175c9f09e576
David Zhou <davidzhou@Davids-MacBook-Pro.local>
2019-06-03 00:33:09 +0800

    Remove duplicate directory

--- Fatigue_main/container/container3/app/dl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-fileid="1bNaOBthTNME64qkeKqNRYdMMogmZZJpc"
-filename="mask_rcnn_coco.h5"
-curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
-
-
