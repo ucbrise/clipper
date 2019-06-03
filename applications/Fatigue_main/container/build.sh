fd3edc00de51e2412c9168db50dc87098a926cf6
cypressd1999 <49230186+cypressd1999@users.noreply.github.com>
2019-06-03 12:04:43 +0800

    Update build.sh

--- applications/Fatigue_main/container/build.sh
+++ applications/Fatigue_main/container/build.sh
@@ -6,7 +6,8 @@ chmod 777 dataDownLoad.sh
 ./dataDownLoad.sh
 tar -xvf part1.tar.gz
 fi
-if [ ! -f ./container2/app/shape_predictor_68_face_landmarks.dat ];
+
+if [ ! -e ./container2/app/shape_predictor_68_face_landmarks.dat ];
 then
 chmod 777 ./container2/app/dl.sh
 ./container2/app/dl.sh
@@ -15,7 +16,9 @@ chmod 777 ./container3/app/dl.sh
 ./container3/app/dl.sh
 mv mask_rcnn_coco.h5 container3/app/
 chmod 777 ./container4/app/dl.sh
-mv 
+mv pose_iter_440000.caffemodel container4/app/
+fi
+
 docker build -f ./Dockerfile_main -t FatigueDetection_main:raft .
 
 
