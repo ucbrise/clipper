3fb4c00aadfbd8fa2e42a3952607a64569f1b342
cypressd1999 <49230186+cypressd1999@users.noreply.github.com>
2019-06-03 11:59:53 +0800

    Update build.sh

--- applications/Fatigue_main/container/build.sh
+++ applications/Fatigue_main/container/build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 #/bin/sh
+if [ ! -e ./part1 ];
+then
 chmod 777 dataDownLoad.sh
 ./dataDownLoad.sh
 tar -xvf part1.tar.gz
+fi
+if [ ! -f ./container2/app/]
 docker build -f ./Dockerfile_main -t FatigueDetection_main:raft .
 
 
