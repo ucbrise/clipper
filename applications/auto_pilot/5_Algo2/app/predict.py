26688b1c3f424ca7ac07482d22f2abb8809af214
Tianhao Zhang <dumpkin1996@gmail.com>
2019-06-03 18:20:01 +0800

    change auto pilot container 5

--- applications/auto_pilot/5_Algo2/app/predict.py
+++ applications/auto_pilot/5_Algo2/app/predict.py
@@ -23,6 +23,10 @@ def read_image(i):
 	image = cv2.imread(image_path)
 	print("original shape", image.shape)
 	return image
+
+global graph, model
+
+graph = tf.get_default_graph()
 	
 model = load_model('/container/Autopilot_V2.h5')
 
@@ -34,7 +38,8 @@ def predict(info):
 		image = read_image(image_index_str)
 		gray = cv2.resize((cv2.cvtColor(image, cv2.COLOR_RGB2HSV))[:, :, 1], (100, 100))
 		print("resized shape", image.shape)
-		steering_angle = keras_predict(model, gray)
+		with graph.as_default():
+			steering_angle = keras_predict(model, gray)
 		end = time.time()
 		print("ELASPSED TIME", end - start)
 		return str(steering_angle) + info
--- applications/auto_pilot/6_Conclusion/app/predict.py
+++ applications/auto_pilot/6_Conclusion/app/predict.py
@@ -6,8 +6,8 @@ def predict(info):
         info0 = info.split("|")[0]
         infn1 = info.split("|")[1]
         result = "algo1 predicts angle " + info0.split("***")[0]
-        result = "algo2 predicts angle " + info1.split("***")[0]
-        result = "planned route is " + info0.split("***")[1]
+        result = "\nalgo2 predicts angle " + info1.split("***")[0]
+        result = "\nplanned route is " + info0.split("***")[1]
         result += "\nobjection detection result is " + info0.split("***")[2]
         return result
     except Exception as exc:
