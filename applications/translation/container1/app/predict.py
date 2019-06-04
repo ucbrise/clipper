d020bfa3bc2357d24740ed4159740a032ed626ec
MXSC <michael.xschen@gmail.com>
2019-06-04 20:08:09 +0800

    wip

--- applications/translation/container1/app/predict.py
+++ applications/translation/container1/app/predict.py
@@ -5,7 +5,43 @@ from timeit import default_timer as timer
 from pocketsphinx import pocketsphinx, Jsgf, FsgModel
 import os
 
-def recognize(audio_file_index, decoder):
+
+
+t1 = timer()
+
+language_directory = "./models/wsj1"
+acoustic_parameters_directory = os.path.join(language_directory, "acoustic-model")
+#language_model_file = os.path.join(language_directory, "language-model.lm")
+language_model_file = os.path.join(language_directory, "language-model.lm.bin")
+
+phoneme_dictionary_file = os.path.join(language_directory, "pronounciation-dictionary.dict")
+
+config = pocketsphinx.Decoder.default_config()
+config.set_string("-hmm", acoustic_parameters_directory)  # set the path of the hidden Markov model (HMM) parameter files
+config.set_string("-lm", language_model_file)
+config.set_string("-dict", phoneme_dictionary_file)
+config.set_string("-logfn", os.devnull)  # disable logging (logging causes unwanted output in terminal)
+# config.get_string("-topn"
+config.set_int("-topn", 1)
+config.set_int("-ds", 4)
+config.set_int("-pl_window", 10)
+config.set_int("-maxhmmpf", 1000)
+config.set_int("-maxwpf", 5)
+
+# print(config.get_int("-maxhmmpf"))
+# config.set_int("-beam", -10000)
+
+decoder = pocketsphinx.Decoder(config)
+
+
+t2 = timer()
+
+print("init time:", t2-t1)
+
+
+
+
+def recognize(audio_file_index):
     audio_file_index = int(audio_file_index)
     if audio_file_index < 0 or audio_file_index > 1000:
         return "Invalid image index! Only index between 1 to 1000 is allowed! Exiting..."
@@ -54,51 +90,23 @@ def recognize(audio_file_index, decoder):
 
     return hypothesis.hypstr
 
-def init_decoder():
-    t1 = timer()
-
-    language_directory = "./models/wsj1"
-    acoustic_parameters_directory = os.path.join(language_directory, "acoustic-model")
-    #language_model_file = os.path.join(language_directory, "language-model.lm")
-    language_model_file = os.path.join(language_directory, "language-model.lm.bin")
-
-    phoneme_dictionary_file = os.path.join(language_directory, "pronounciation-dictionary.dict")
-
-    config = pocketsphinx.Decoder.default_config()
-    config.set_string("-hmm", acoustic_parameters_directory)  # set the path of the hidden Markov model (HMM) parameter files
-    config.set_string("-lm", language_model_file)
-    config.set_string("-dict", phoneme_dictionary_file)
-    config.set_string("-logfn", os.devnull)  # disable logging (logging causes unwanted output in terminal)
-    
-    decoder = pocketsphinx.Decoder(config)
-
-
-    t2 = timer()
-
-    print("init time:", t2-t1)
-    return decoder
-
 
 def predict(audio_file_path):
     start = timer()
     recognized_string = recognize(audio_file_path)
+    print(recognized_string)
     end = timer()
     time_elapsed = end - start
     return recognized_string
 
 
-if __name__ == "__main__":
-    d = init_decoder()
-    # print(recognize(1, d))
-    # print("\n\n")
-    # print(recognize(2, d))
 
-    lines = []
-    for i in range (0, 1000):
-        lines.append(recognize(i, d))
+    # lines = []
+    # for i in range (0, 1000):
+    #     lines.append(recognize(i, d))
     
-    f = open("output.txt", "w")
+    # f = open("output.txt", "w")
 
-    for line in lines:
-        f.write(line + '\n')
-    f.close()
+    # for line in lines:
+    #     f.write(line + '\n')
+    # f.close()
