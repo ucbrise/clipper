896d8bf2496cdd312d5a5eb0a0d125192972fc61
Simon Mo <xmo@berkeley.edu>
2019-06-11 19:36:15 -0700

    Cleanup with more keywords

--- bin/cleanup_jenkins.sh
+++ bin/cleanup_jenkins.sh
@@ -7,22 +7,24 @@ CLIPPER_REGISTRY=clippertesting
 # best effort, ignore all errors
 try_cleanup() {
     (docker ps --all | grep "$@" | awk '{ print $1 }' | xargs docker kill) || true
-    (docker container ls --all | grep "$@" | awk '{ print $3 }' | xargs docker container rm) || true
-    (docker image ls --all | grep "$@" | awk '{ print $3 }' | xargs docker image rm -f) || true
-    docker image prune -f
 }
 
-try_cleanup_docker_volume() {
-    (docker volume ls -f dangling=true | awk '{ print $2 }' | xargs docker volume rm -f) || true
-}
+KEYWORDS = (
+  ${CLIPPER_REGISTRY}
+  clipper
+  redis
+  model
+  prometheus
+  fluentd
+)
 
 echo "Docker Status Before"
 docker ps
 
-try_cleanup ${CLIPPER_REGISTRY} 
-try_cleanup clipper 
-try_cleanup shipyard 
-try_cleanup_docker_volume 
+for i in "${KEYWORDS[@]}"
+do
+  try_cleanup "$i"
+done
 
 echo "Docker Status After"
 docker ps
