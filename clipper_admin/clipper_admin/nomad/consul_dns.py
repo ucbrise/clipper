37196ef84278db1f067a9e552f273f854b5e0c49
Antoine Sauray <sauray.antoine@gmail.com>
2019-10-17 16:24:06 +0200

    add comments in dns.py and consul_dns.py

--- clipper_admin/clipper_admin/nomad/consul_dns.py
+++ clipper_admin/clipper_admin/nomad/consul_dns.py
@@ -2,10 +2,19 @@ from .dns import DNS
 import dns.resolver
 import socket
 
+"""
+    Consul is a service networking solution to connect and secure services across any runtime platform and public or private cloud
+"""
 class ConsulDNS(DNS):
 
-    def resolveSRV(self, job_name):
-        addr = '{}.service.consul'.format(job_name)
+    """
+        This method resolves records of IP and Ports with a SRV DNS request
+        Parameters:
+            domain str:
+                The domain to resolve, in Consul this correspond to the healthcheck name
+    """
+    def resolveSRV(self, check_name):
+        addr = '{}.service.consul'.format(check_name)
         srv_records= dns.resolver.query(addr, 'SRV')
         srvInfo = {}
         for srv in srv_records:
--- clipper_admin/clipper_admin/nomad/dns.py
+++ clipper_admin/clipper_admin/nomad/dns.py
@@ -2,7 +2,13 @@ import abc
 from abc import abstractmethod
 class DNS(abc.ABC):
 
+    """
+        This method resolves records of IP and Ports with a SRV DNS request
+        Parameters:
+            domain str:
+                The domain to resolve
+    """
     @abstractmethod
-    def resolveSRV(self, job_name):
+    def resolveSRV(self, domain):
         pass
     
